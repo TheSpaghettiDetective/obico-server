db224b8f00c1e2c469bb4c584e4b6c330c30bf7b
Kenneth Jiang <kenneth.jiang@gmail.com>
2019-01-23 15:38:04 -0800

    Token auth for ml_api

---
+++ ml_api/auth.py
@@ -0,0 +1,22 @@
+import os
+from functools import wraps
+from flask import Flask, request, Response
+
+ML_API_TOKEN=os.environ.get("ML_API_TOKEN")
+
+def token_required(f):
+    @wraps(f)
+    def check_authorization(*args, **kwargs):
+        if request.headers.get("Authorization") == 'Bearer: {}'.format(ML_API_TOKEN):
+            return f()
+        else:
+            return Response(status=401)
+
+    @wraps(f)
+    def passthru(*args, **kwargs):
+        return f()
+
+    if ML_API_TOKEN:
+        return check_authorization
+    else:
+        return passthru

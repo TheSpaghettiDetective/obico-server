f7452819cced427d119d4814ed077937253cd238
Kenneth Jiang <kenneth.jiang@gmail.com>
2019-02-02 21:44:16 -0800

    Django connection pooling

--- web/config/settings.py
+++ web/config/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 import dj_database_url
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 # Password validation

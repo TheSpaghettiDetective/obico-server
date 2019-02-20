6a4a7fc28b74253a2fc171165edc9c9b714ca27c
Kenneth Jiang <kenneth.jiang@gmail.com>
2019-02-20 08:44:53 -0800

    add alert time to print export

--- web/app/management/commands/extract_prints_from_hist.py
+++ web/app/management/commands/extract_prints_from_hist.py
@@ -13,6 +13,7 @@ class Command( BaseCommand ):
         for printer_id in set([k['id'] for k in HistoricalPrinter.objects.values('id')]):
             started = None
             print_filename = None
+            alerted_at = None
             for hist in HistoricalPrinter.objects.filter(id=printer_id).order_by('history_id'):
                 if hist.current_print_filename:
                     if not started or print_filename != hist.current_print_filename:
@@ -20,9 +21,21 @@ class Command( BaseCommand ):
                         started = hist.current_print_started_at
                 else:
                     if started:
-                        prints += [dict(filename=print_filename, start=started, end=hist.history_date, start_secs=int(started.timestamp()), end_secs=int(hist.history_date.timestamp()), id=hist.id)]
+                        prints += [dict(
+                            filename=print_filename,
+                            start=started,
+                            end=hist.history_date,
+                            alert=alerted_at,
+                            start_secs=int(started.timestamp()),
+                            end_secs=int(hist.history_date.timestamp()),
+                            alert_secs=int(alerted_at.timestamp()) if alerted_at else '',
+                            id=hist.id)]
                         started = None
                         print_filename = None
+                        alerted_at = None
+                if hist.current_print_alerted_at and started:
+                        alerted_at = hist.current_print_alerted_at
+
         w = csv.DictWriter(sys.stdout, prints[0].keys())
         w.writeheader()
         w.writerows(prints)

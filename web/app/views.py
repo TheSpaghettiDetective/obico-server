9669e3978ed4d2b0a949c570566be10b135dc0dc
Kenneth Jiang <kenneth.jiang@gmail.com>
2019-03-06 07:07:55 -0800

    more reverts

--- web/app/views.py
+++ web/app/views.py
@@ -129,10 +129,10 @@ def phone_token_validation(request):
 
 @login_required
 def prints(request):
-    prints = list(Print.objects.filter(printer__user=request.user).order_by('-id').values())
+    prints = Print.objects.filter(printer__user=request.user).order_by('-id')
     page_obj = get_paginator(prints, request, 9)
-    prediction_urls = [ dict(print_id=print.id, prediction_json_url=print.prediction_json_url) for print in prints]
-    return render(request, 'print_list.html', dict(prints=prints, page_obj=page_obj, prediction_urls=prediction_urls))
+    prediction_urls = [ dict(print_id=print.id, prediction_json_url=print.prediction_json_url) for print in page_obj.object_list]
+    return render(request, 'print_list.html', dict(prints=page_obj.object_list, page_obj=page_obj, prediction_urls=prediction_urls))
 
 def publictimelapse_list(request):
     timelapses_list = list(PublicTimelapse.objects.order_by('priority').values())

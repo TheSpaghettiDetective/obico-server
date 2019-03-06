6341f3f6bbbae67afe565060d4af06b425ee23f6
Kenneth Jiang <kenneth.jiang@gmail.com>
2019-03-06 15:35:09 -0800

    maybe debug_task is useful?

--- web/config/celery.py
+++ web/config/celery.py
@@ -15,3 +15,7 @@ celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 celery_app.autodiscover_tasks()
+
+@celery_app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))

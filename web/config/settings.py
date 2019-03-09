4e7b9c1ed0f808fdfc706ae3a2c9936d2610b89c
Kenneth Jiang <kenneth.jiang@gmail.com>
2019-03-09 06:05:22 -0800

    use env var to configure redis url for channels layer

--- web/config/settings.py
+++ web/config/settings.py
@@ -268,7 +268,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('redis', 6379)],
+            "hosts": [REDIS_URL],
         },
     },
 }

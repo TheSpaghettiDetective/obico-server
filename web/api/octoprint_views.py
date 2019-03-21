b66e22e017883d8b82d03218ebda4919da7ccc51
Kenneth Jiang <kenneth.jiang@gmail.com>
2019-03-21 11:24:20 -0700

    emporarily disable pause-on-failure so that we have time to fix the resume bug

--- web/api/octoprint_views.py
+++ web/api/octoprint_views.py
@@ -30,7 +30,8 @@ def alert_if_needed(printer):
 
     printer.set_alert()
 
-    pause_print = printer.action_on_failure == Printer.PAUSE
+    # pause_print = printer.action_on_failure == Printer.PAUSE
+    pause_print = False # Temporarily disable pause-on-failure so that we have time to fix the resume bug
     if pause_print:
         printer.pause_print_on_failure()
 

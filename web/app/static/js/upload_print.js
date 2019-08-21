aaf9548e9f33f1ef5c9dbdf1688b3a9668b4a571
Kenneth Jiang <kenneth.jiang@gmail.com>
2019-08-21 12:14:08 -0700

    More user feedback for large file upload

--- web/app/static/js/upload_print.js
+++ web/app/static/js/upload_print.js
@@ -5,12 +5,19 @@ var dgEarned = 0;
 
 Dropzone.options.printUpload = {
     paramName: "file", // The name that will be used to transfer the file
+    withCredentials: true,
     maxFilesize: 200, // MB
+    timeout: 60*60*1000, // For large files
     acceptedFiles: "video/mp4,video/mpeg",
     success: function (file) {
+        $('#tl-verifying').hide();
         $('#tl-uploaded').show();
         dgEarned += 4;
         $('#dg-earned').html('You earned ' + '<img class="dg-icon" src="/static/img/detective-gear-inverse.png" />'.repeat(dgEarned));
-    }
+    },
+    uploadprogress: function (file, progress) {
+        $('#tl-uploaded').hide();
+        $('#tl-verifying').show();
+    },
 };
 

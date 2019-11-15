5ff248842da3a59d6cb63951725b7233c59a5fb7
Kenneth Jiang <kenneth.jiang@gmail.com>
2019-11-14 20:28:28 -0800

    dh_balance field has been moved to db

--- web/lib/redis.py
+++ web/lib/redis.py
@@ -7,9 +7,6 @@ REDIS = redis.Redis.from_url(settings.REDIS_URL, charset="utf-8", decode_respons
 def printer_key_prefix(printer_id):
     return 'printer:{}:'.format(printer_id)
 
-def user_key_prefix(user_id):
-    return 'user:{}:'.format(user_id)
-
 def print_key_prefix(print_id):
     return 'print:{}:'.format(print_id)
 
@@ -65,14 +62,6 @@ def printer_p_json_set(printer_id, pic_id, json_str, ex=None):
 def printer_p_json_get(printer_id, pic_id):
     key = f'{printer_key_prefix(printer_id)}:p_json:{pic_id}'
     return REDIS.get(key)
-
-def user_dh_balance_set(user_id, balance):
-    key = f'{user_key_prefix(user_id)}:dh'
-    REDIS.set(key, balance)
-
-def user_dh_balance_get(user_id):
-    key = f'{user_key_prefix(user_id)}:dh'
-    return float(REDIS.get(key) or 0)
 
 def print_num_predictions_incr(print_id):
     key = f'{print_key_prefix(print_id)}:pred'

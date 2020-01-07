eb3c0a70088d7066719d79097eb780c6ba3adfde
Kenneth Jiang <kenneth.jiang@gmail.com>
2020-01-06 19:40:10 -0800

    Give realtime task a dedicated queue instead

--- web/config/celery.py
+++ web/config/celery.py
@@ -24,8 +24,13 @@ celery_app.conf.task_ignore_result = True
 celery_app.conf.task_store_errors_even_if_ignored = True
 celery_app.conf.broker_transport_options = {'visibility_timeout': 3600*12}
 celery_app.conf.task_routes = {
-    'app.tasks.prune_channel_presence': {'queue': 'slow_tasks'},
-    'app.tasks.compile_timelapse': {'queue': 'slow_tasks'},
+    'app.tasks.print_notification': {'queue': 'realtime'},
+    'app.tasks.process_print_events': {'queue': 'realtime'},
+    'app_ent.tasks.credit_dh_for_contribution': {'queue': 'realtime'},
+    'app_ent.tasks.process_print_events_ent': {'queue': 'realtime'},
+    'app_ent.tasks.send_welcome_email': {'queue': 'realtime'},
+    'app_ent.tasks.set_coturn_credential': {'queue': 'realtime'},
+    'app_ent.tasks.sync_account_status': {'queue': 'realtime'},
 }
 
 # Using a string here means the worker doesn't have to serialize

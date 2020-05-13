db7d4e47222ef6beab0290afa97cf3174c3b34af
encetamasb <encetamasb@gmail.com>
2020-05-13 11:26:28 +0200

    Added a simple vue demo app

---
+++ web/frontend/src/simple/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  <div class="example">{{ msg }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello from vue!'
+    }
+  }
+}
+</script>
+
+<style>
+.example {
+  color: red;
+}
+</style>

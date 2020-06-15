3418ee7f408fdafdfb93f66587c3cccdcad381b1
encetamasb <encetamasb@gmail.com>
2020-06-15 15:07:26 +0200

    Separated eslint config into standalone file; downgraded to vue/essentials

---
+++ web/frontend/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    // this one is stricter: 'plugin:vue/strongly-recommended',
+    'plugin:vue/essential',
+    'eslint:recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+    parser: 'babel-eslint'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+  }
+}

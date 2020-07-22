f229b9d14f0a0f29616020a27aa3757813641969
encetamasb <encetamasb@gmail.com>
2020-07-22 16:20:30 +0200

    Added localStorage and DurationBlock helpers

--- web/frontend/src/lib/printers.js
+++ web/frontend/src/lib/printers.js
@@ -1,4 +1,5 @@
 import get from 'lodash/get'
+import moment from 'moment'
 
 
 let isPrinterIdle = (printerState) =>
@@ -10,7 +11,7 @@ let isPrinterPaused = (printerState) =>
 let isPrinterDisconnected = (printerState) =>
   get(printerState, 'flags.closedOrError', true)
 
-let printerHasError = (printerState) => 
+let printerHasError = (printerState) =>
   get(printerState, 'flags.error') ||
   get(printerState, 'text', '').toLowerCase().includes('error')
 
@@ -18,10 +19,69 @@ let printInProgress = (printerState) =>
   !isPrinterDisconnected(printerState) &&
   get(printerState, 'text', '') !== 'Operational'
 
+
+let getLocalPref = (prefId, defaultValue) => {
+  var val = localStorage.getItem(prefId) || defaultValue
+  // Hack to deal with data type such as boolean and number
+  try {
+    return JSON.parse(val)
+  } catch (e) {
+    return val
+  }
+}
+
+let setLocalPref = (prefId, value) => {
+  return localStorage.setItem(prefId, value)
+}
+
+let getPrinterLocalPref = (prefix, printerId, defaultValue) => {
+  var itemId = prefix + String(printerId)
+  var val = localStorage.getItem(itemId) || defaultValue
+  try {
+    return JSON.parse(val)
+  } catch (e) {
+    return val
+  }
+}
+
+let setPrinterLocalPref = (prefix, printerId, value) => {
+  var itemId = prefix + String(printerId)
+  return localStorage.setItem(itemId, value)
+}
+
+let toDuration = (seconds, printerState) => {
+  if (seconds == null || seconds == 0) {
+    return {
+      valid: false,
+      printing: printInProgress(printerState),
+    }
+  } else {
+    var d = moment.duration(seconds, 'seconds')
+    var h = Math.floor(d.asHours())
+    var m = d.minutes()
+    var s = d.seconds()
+    return {
+      valid: true,
+      printing: true,
+      hours: h,
+      showHours: (h>0),
+      minutes: m,
+      showMinutes: (h>0 || m>0),
+      seconds: s,
+      showSeconds: (h==0 && m==0)
+    }
+  }
+}
+
 export {
   isPrinterIdle,
   isPrinterPaused,
   isPrinterDisconnected,
   printerHasError,
-  printInProgress
+  printInProgress,
+  getLocalPref,
+  setLocalPref,
+  getPrinterLocalPref,
+  setPrinterLocalPref,
+  toDuration,
 }

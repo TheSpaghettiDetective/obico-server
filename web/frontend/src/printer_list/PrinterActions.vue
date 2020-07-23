7494e6502d00978b2775f67c65af94f4e8cf6753
encetamasb <encetamasb@gmail.com>
2020-07-23 14:49:13 +0200

    Converted printer_actions mustache template

---
+++ web/frontend/src/printer_list/PrinterActions.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="row my-2">
+    <div
+      v-if="status"
+    >
+      <div
+        v-if="error"
+      >
+        <div class="col-12 bg-danger text-center">
+          <div>OctoPrint Error</div><div>{{ printerStateTxt }}</div>
+        </div>
+      </div>
+
+      <div
+        v-if="!disconnected"
+      >
+        <div
+          v-if="!idle"
+        >
+          <div class="col-sm-6">
+            <button
+              id="print-pause-resume"
+              type="button"
+              class="btn btn-block mt-2"
+              :class="{'btn-outline-warning': !printerPaused, 'btn-outline-success': printerPaused}"
+              @click="onPauseToggled()"
+            >
+              <span
+                v-if="!printerPaused"
+              >
+                <i class="fas fa-pause"></i>&nbsp;&nbsp;Pause
+              </span>
+
+              <span
+                v-if="printerPaused"
+              >
+                <i class="fas fa-play"></i>&nbsp;&nbsp;Resume
+              </span>
+            </button>
+          </div>
+          <div class="col-sm-6">
+            <button
+              id="print-cancel"
+              type="button"
+              class="btn btn-outline-danger btn-block mt-2 mb-2"
+              @click="$emit('PrinterActionCancelClicked')"
+            >
+              <i class="fas fa-stop"></i>&nbsp;&nbsp;Cancel
+            </button>
+          </div>
+        </div>
+        <div
+          v-if="idle"
+        >
+          <div class="col-sm-6">
+            <button
+              id="start-print"
+              type="button"
+              class="btn btn-outline-primary btn-block mt-2"
+              @click="$emit('PrinterActionStartClicked')"
+            >
+              <i class="fas fa-star"></i>&nbsp;&nbsp;Start Printing
+            </button>
+          </div>
+          <div class="col-sm-6">
+            <a
+              type="button"
+              role="button"
+              class="btn btn-outline-secondary btn-block mt-2 mb-2"
+              :href="controlUrl"
+              @click="$emit('PrinterActionControlClicked')"
+            >
+              <i class="fas fa-arrows-alt"></i>&nbsp;&nbsp;Control
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div
+        v-if="disconnected"
+      >
+        <div class="col-12 text-center py-2 text-warning">
+          <div>Printer is not connected to OctoPrint.</div>
+          <button
+            id="connect-printer"
+            type="button"
+            class="btn btn-outline-primary btn-block mt-2"
+            @click="$emit('PrinterActionConnectClicked')"
+            :disabled="isConnecting"
+          >
+            <i class="fab fa-usb"></i>&nbsp;&nbsp;Connect
+            <i
+              v-if="connecting"
+              class="fas fa-spinner fa-spin"
+            ></i>
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div
+      v-if="!status"
+    >
+      <div class="col-12 text-center py-3 text-warning">
+        <div>
+          OctoPrint is not powered on, or is offline.
+          <a href="https://www.thespaghettidetective.com/docs/octoprint-is-offline/">Why?</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PrinterActions',
+  props: {
+    printerId: {},
+    status: {},
+    printerStateTxt: {},
+    printerPaused: {},
+    idle: {},
+    error: {},
+    disconnected: {},
+
+    // --
+
+    connecting: {},
+  },
+  computed: {
+  },
+  methods: {
+    onPauseToggled() {
+      if (this.printerPaused) {
+        this.$emit('PrinterActionResumeClicked')
+      } else {
+        this.$emit('PrinterActionPauseClicked')
+      }
+    },
+  }
+}
+</script>

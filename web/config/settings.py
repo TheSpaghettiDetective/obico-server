b4da6bf4607dc0d330b4ed9df444d316e1d31440
chand1012 <chandlerl2000@gmail.com>
2020-07-28 16:38:49 -0400

    added database columns

--- web/config/settings.py
+++ web/config/settings.py
@@ -117,7 +117,7 @@ MESSAGE_TAGS = {
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(conn_max_age=600, default='sqlite:///db.sqlite3'),
 }
 
 # Password validation

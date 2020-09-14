949d262c06807b25736998d512dcda5603a63163
Kenneth Jiang <kenneth.jiang@gmail.com>
2020-09-14 07:04:42 -0700

    Coturn credential is not set in a task anymore

--- web/config/celery.py
+++ web/config/celery.py
@@ -30,7 +30,6 @@ celery_app.conf.task_routes = {
     'app_ent.tasks.credit_dh_for_contribution': {'queue': 'realtime'},
     'app_ent.tasks.process_print_events_ent': {'queue': 'realtime'},
     'app_ent.tasks.send_welcome_email': {'queue': 'realtime'},
-    'app_ent.tasks.set_coturn_credential': {'queue': 'realtime'},
     'app_ent.tasks.send_free_trial_expiring_message': {'queue': 'realtime'},
 }
 

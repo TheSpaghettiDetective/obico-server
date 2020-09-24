52fd3457282ae5905a661f00a3cde44c53e37c89
Kenneth Jiang <kenneth.jiang@gmail.com>
2020-09-24 10:04:06 -0700

    Refactor to put all views in a folder

--- web/app/urls.py
+++ web/app/urls.py
@@ -1,41 +1,44 @@
 from django.urls import path, re_path
 
-from . import views
-from . import tunnel
-from . import vue_demo
+from .views import web_views
+from .views import tunnel_views
+from .views import mobile_views
+from .views import vue_demo
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('consent/', views.consent, name='consent'),
-    path('media/<path:file_path>', views.serve_jpg_file, name='serve_jpg_file'),  # semi hacky solution to serve image files
-    path('printer_auth_token/<int:pk>/', views.printer_auth_token, name='printer_auth_token'),
-    path('printers/', views.printers, name='printers'),
-    path('printers/<pk>/', views.edit_printer, name='edit_printer'),
-    path('printers/<int:pk>/delete/', views.delete_printer, name='delete_printer'),
-    path('printers/<int:pk>/share/', views.share_printer, name='share_printer'),
-    path('printers/<int:pk>/control/', views.control_printer, name='control_printer'),
-    path('printers/<int:pk>/integration/', views.integration, name='integration'),
-    path('printers/shared/<share_token>/', views.printer_shared, name='printer_shared'),
-    path('publictimelapses/', views.publictimelapse_list, name='publictimelapse_list'),
-    path('user_preferences/', views.user_preferences, name='user_preferences'),
-    path('test_telegram', views.test_telegram, name='test_telegram'),
-    path('unsubscribe_email/', views.unsubscribe_email, name='unsubscribe_email'),
-    path('prints/<int:pk>/cancel/', views.cancel_print, name='cancel_print'),
-    path('prints/<int:pk>/resume/', views.resume_print, name='resume_print'),
-    path('prints/', views.prints, name='prints'),
-    path('prints/upload/', views.upload_print, name='upload_print'),
-    path('prints/<pk>/', views.print, name='print'),
-    path('prints/shot-feedback/<pk>/', views.print_shot_feedback, name='print_shot_feedback'),
-    path('gcodes/', views.gcodes, name='gcodes'),
-    path('gcodes/upload/', views.upload_gcode_file, name='upload_gcode_file'),
-    path('secure_redirect/', views.secure_redirect, name='secure_redirect'),
+    path('', web_views.index, name='index'),
+    path('consent/', web_views.consent, name='consent'),
+    path('media/<path:file_path>', web_views.serve_jpg_file, name='serve_jpg_file'),  # semi hacky solution to serve image files
+    path('printer_auth_token/<int:pk>/', web_views.printer_auth_token, name='printer_auth_token'),
+    path('printers/', web_views.printers, name='printers'),
+    path('printers/<pk>/', web_views.edit_printer, name='edit_printer'),
+    path('printers/<int:pk>/delete/', web_views.delete_printer, name='delete_printer'),
+    path('printers/<int:pk>/share/', web_views.share_printer, name='share_printer'),
+    path('printers/<int:pk>/control/', web_views.control_printer, name='control_printer'),
+    path('printers/<int:pk>/integration/', web_views.integration, name='integration'),
+    path('printers/shared/<share_token>/', web_views.printer_shared, name='printer_shared'),
+    path('publictimelapses/', web_views.publictimelapse_list, name='publictimelapse_list'),
+    path('user_preferences/', web_views.user_preferences, name='user_preferences'),
+    path('test_telegram', web_views.test_telegram, name='test_telegram'),
+    path('unsubscribe_email/', web_views.unsubscribe_email, name='unsubscribe_email'),
+    path('prints/<int:pk>/cancel/', web_views.cancel_print, name='cancel_print'),
+    path('prints/<int:pk>/resume/', web_views.resume_print, name='resume_print'),
+    path('prints/', web_views.prints, name='prints'),
+    path('prints/upload/', web_views.upload_print, name='upload_print'),
+    path('prints/<pk>/', web_views.print, name='print'),
+    path('prints/shot-feedback/<pk>/', web_views.print_shot_feedback, name='print_shot_feedback'),
+    path('gcodes/', web_views.gcodes, name='gcodes'),
+    path('gcodes/upload/', web_views.upload_gcode_file, name='upload_gcode_file'),
+    path('secure_redirect/', web_views.secure_redirect, name='secure_redirect'),
+
     re_path(r'^octoprint/(?P<printer_id>\d+)',
-        tunnel.octoprint_http_tunnel,
+        tunnel_views.octoprint_http_tunnel,
         name='octoprint_http_tunnel'),
-    path('tunnel/<int:printer_id>/', tunnel.tunnel, name='tunnel'),
-    path('mobile/login/', views.MobileLoginView.as_view(), name='mobile_login'),
-    path('mobile/fetch/', views.mobile_fetch, name='mobile_fetch'),
-    path('mobile/callback/', views.callback, name='mobile_callback'),
+    path('tunnel/<int:printer_id>/', tunnel_views.tunnel, name='tunnel'),
+
+    path('mobile/auth/login/', mobile_views.MobileLoginView.as_view(), name='mobile_auth_login'),
+    path('mobile/auth/fetch/', mobile_views.fetch_session, name='mobile_auth_fetch_session'),
+    path('mobile/auth/oauth_callback/', mobile_views.oauth_callback, name='mobile_auth_oauth_callback'),
 
     # vue demo urls
     path('vue-demo/simple/', vue_demo.SimpleAppView.as_view(), name='vue-demo-simple'),
--- web/app/vue_demo.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic import TemplateView
-
-
-class SimpleAppView(TemplateView):
-    template_name = "vue-demo/simple.html"
-
-
-class MultiAppView(TemplateView):
-    template_name = "vue-demo/multi.html"

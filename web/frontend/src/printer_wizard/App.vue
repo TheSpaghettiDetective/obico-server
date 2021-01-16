1c2efe12b6f477a317b311b8bb3001c808e0bf78
Puviez <boredkeeper1793@gmail.com>
2021-01-16 10:21:17 +0800

    Page 3 UI

--- web/frontend/src/printer_wizard/App.vue
+++ web/frontend/src/printer_wizard/App.vue
@@ -21,7 +21,36 @@
       </div>
       <div class="helper col ml-auto">*Need help? Check out the step by step <a href="#">set up guide</a></div>
     </div>
-    <div v-else-if="setupStage === 'link'">LINK</div>
+    <div v-else-if="setupStage === 'link'" class="container">
+      <div class="col"></div>
+      <div class="d-flex flex-column col">
+        <b-row class="pt-3 text-center">
+          <div class="mx-auto title pb-2">Link Your Printer</div>
+        </b-row>
+        <b-row class="pt-1 text-center">
+          <div class="mx-auto subtitle pb-3">Copy the 6-Digit verification Code</div>
+        </b-row>
+        <b-row class="d-flex justify-content-center">
+          <div class="px-1">
+            <b-button class="special-btn code-btn" @click="copy">{{ verificationCode }}</b-button>
+          </div>
+          <div class="px-1">
+            <b-button class="special-btn copy-btn" @click="copy" variant="primary">Copy</b-button>
+          </div>
+        </b-row>
+        <b-row class="pt-1 text-center">
+          <div class="mx-auto pb-3">{{ `*This code will expire in ${validityHours} hrs ${validityMins} mins` }}</div>
+        </b-row>
+        <b-row class="pt-1 d-flex flex-column">
+          <div class="mx-auto subtitle">Enter the 6-Digit verification Code in the Plugin</div>
+          <div class="helper mx-auto pb-1" style="max-width: 220px;">Can't find where to enter the code?<span class="px-1" v-b-tooltip.hover.right="{ variant: 'primary' }" title="You need the latest version of TSD plugin">ⓘ</span></div>
+        </b-row>
+        <b-row class="pb-3 d-flex justify-content-center">
+          <img :src="require('@static/img/TSDVerificationScreenshot.png')">
+        </b-row>
+      </div>
+      <div class="helper col ml-auto">*Need help? Check out the step by step <a href="#">set up guide</a></div>
+    </div>
   </div>
 </template>
 
@@ -34,8 +63,24 @@ export default {
   },
   data() {
     return {
-      setupStage: 'install'
+      setupStage: 'install',
+      verificationCode: 313894,
+      validityTime: 3600000
     }
+  },
+  computed: {
+    validityHours() {
+      return `0${Math.floor(this.validityTime / 3600000)}`.slice(-2)
+    },
+    validityMins() {
+      return `0${(Math.floor(this.validityTime / 60000) % 60)}`.slice(-2)
+    }
+  },
+  created() {
+    setInterval(() => {
+      this.validityTime -= 5000
+      console.log(this.validityTime)
+    }, 5000)
   },
   mounted() {
     this.getStage()
@@ -43,11 +88,15 @@ export default {
   methods: {
     getStage() {
       const params = new URLSearchParams(window.location.search)
-      const stage = params.get('setup')
+      const stage = params.get('setup') 
       this.setupStage = stage
+      console.log('Stage Set!')
     },
     toLinkStage() {
       this.setupStage = 'link'
+    },
+    copy() {
+      return
     }
   }
 }
@@ -57,7 +106,7 @@ export default {
 @use "~main/theme"
 
 img
-  width: 35vw
+  width: 30vw
   min-width: 360px
 
 .spacer
@@ -67,11 +116,29 @@ img
   font-size: 2.5rem
   font-weight: 800
 
+.subtitle
+  font-size: 1.5rem
+  font-weight: 500
+
 .btn
   width: 220px
   height: 60px
   font-size: 1.3rem
   line-height: 1.3rem
+
+.special-btn
+  border-radius: 10px
+
+.code-btn
+  background-color: black
+  border: black
+  color: white
+  font-size: 2rem
+  font-weight: 500
+  letter-spacing: 2px
+
+.copy-btn
+  width: 90px
 
 .content
   font-size: 1.2rem

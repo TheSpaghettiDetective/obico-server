8ed25b6c49eb2ab1d37a66009cfedb40609be74d
Kenneth Jiang <kenneth.jiang@gmail.com>
2021-01-17 08:23:42 -0800

    Save work for now

--- web/frontend/src/printer_wizard/main.js
+++ web/frontend/src/printer_wizard/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import { BootstrapVue} from 'bootstrap-vue'
-import App from './PrinterWizard.vue'
+import App from './PrinterWizardPage.vue'
 import setupSentry from '@lib/sentry'
 
 setupSentry(Vue)

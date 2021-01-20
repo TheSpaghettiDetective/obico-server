8e32bb6fc4f4f0481a661a3c8846bca187dc7ddc
Dmitry Puzyrev <puzyrev.dmitry@gmail.com>
2021-01-20 17:43:57 +0300

    Implement settings page on VueJS

--- web/frontend/src/printers/PrinterPreferences.vue
+++ web/frontend/src/printers/PrinterPreferences.vue
@@ -1,30 +1,33 @@
 <template>
   <div>
+    <!-- Potential failure section -->
     <div class="failure-notification">
       <div class="form-group mt-4 mb-4">
         <div class="form-label text-muted">When a potential failure is detected:</div>
-        <div class="custom-control custom-radio mt-1">
+        <div class="custom-control custom-radio mt-1 input-wrapper radio" id="pauseAndNotifyFalse">
           <input
             type="radio"
             name="action_on_failure"
             class="custom-control-input field_required"
             id="id_action_on_failure_0"
             :checked="!pauseAndNotify"
-            @change="$emit('pauseAndNotifyChanged', false)">
+            @change="emitPauseAndNotify($event, false)">
           <label class="custom-control-label" for="id_action_on_failure_0">Just notify me</label>
         </div>
-        <div class="custom-control custom-radio mt-1">
+        <div class="custom-control custom-radio mt-1 input-wrapper radio" id="pauseAndNotifyTrue">
           <input
             type="radio"
             name="action_on_failure"
             class="custom-control-input field_required"
             id="id_action_on_failure_1"
             :checked="pauseAndNotify"
-            @change="$emit('pauseAndNotifyChanged', true)">
+            @change="emitPauseAndNotify($event, true)">
           <label class="custom-control-label" for="id_action_on_failure_1">Pause the printer and notify me</label>
         </div>
       </div>
     </div>
+
+    <!-- Advanced settings section (toggle) -->
     <div class="advanced">
       <div class="accordion">
         <div class="card" style="border: none;">
@@ -41,40 +44,42 @@
                 <i class="fas fa-exclamation-triangle"></i>
                 If you are not sure about the settings below, leave the default values to minimize surprises.
               </p>
+
+              <!-- Advanced settngs: when printer is paused -->
               <div class="form-group mt-4">
                 <div class="form-label text-muted">When print is paused,</div>
-                <div class="custom-control custom-checkbox form-check-inline mt-2">
+                <div class="custom-control custom-checkbox form-check-inline mt-2 input-wrapper checkbox" id="isHotendHeaterOff">
                   <input
                     type="checkbox"
                     name="tools_off_on_pause"
                     class="custom-control-input"
                     id="id_tools_off_on_pause"
                     :checked="isHotendHeaterOff"
-                    @change="$emit('isHotendHeaterOffChanged', $event.target.checked)">
+                    @change="emitIsHotendHeaterOff($event)">
                   <label class="custom-control-label" for="id_tools_off_on_pause">
                     Turn off hotend heater(s)
                   </label>
                 </div>
-                <div class="custom-control custom-checkbox form-check-inline mt-2">
+                <div class="custom-control custom-checkbox form-check-inline mt-2 input-wrapper checkbox" id="isBedHeaterOff">
                   <input
                     type="checkbox"
                     name="bed_off_on_pause"
                     class="custom-control-input"
                     id="id_bed_off_on_pause"
                     :checked="isBedHeaterOff"
-                    @change="$emit('isBedHeaterOffChanged', $event.target.checked)">
+                    @change="emitIsBedHeaterOff($event)">
                   <label class="custom-control-label" for="id_bed_off_on_pause">
                     Turn off bed heater
                   </label>
                 </div>
-                <div class="form-inline my-1">
+                <div class="form-inline my-1 input-wrapper checkbox-with-input" id="retractFilamentBy">
                   <div class="custom-control custom-checkbox form-check-inline">
                     <input
                       type="checkbox"
                       class="custom-control-input"
                       id="retract-checkbox"
                       :checked="retractFilamentBy !== false"
-                      @change="$event.target.checked ? $emit('retractFilamentByChanged', retractFilamentByInput) : $emit('retractFilamentByChanged', false)">
+                      @change="emitRetractFilamentBy($event)">
                     <label class="custom-control-label" for="retract-checkbox">Retract filament by</label>
                   </div>
                   <div class="input-group input-group-sm minimal-width">
@@ -87,20 +92,20 @@
                       class="form-control field_required"
                       id="id_retract_on_pause"
                       v-model="retractFilamentByInput"
-                      @change="EmitIfEtractFilamentBy()">
+                      @change="EmitIfEtractFilamentBy($event)">
                     <div class="input-group-append">
                       <span class="input-group-text">mm</span>
                     </div>
                   </div>
                 </div>
-                <div class="form-inline my-1">
+                <div class="form-inline my-1 input-wrapper checkbox-with-input" id="liftExtruderBy">
                   <div class="custom-control custom-checkbox form-check-inline">
                     <input
                       type="checkbox"
                       class="custom-control-input"
                       id="lift-z-checkbox"
                       :checked="liftExtruderBy !== false"
-                      @change="$event.target.checked ? $emit('liftExtruderByChanged', liftExtruderByInput) : $emit('liftExtruderByChanged', false)">
+                      @change="emitLiftExtruderBy($event)">
                     <label class="custom-control-label" for="lift-z-checkbox">Lift extruder along Z axis by</label>
                   </div>
                   <div class="input-group input-group-sm minimal-width">
@@ -113,16 +118,18 @@
                       class="form-control field_required"
                       id="id_lift_z_on_pause"
                       v-model="liftExtruderByInput"
-                      @change="EmitIfLiftExtruderBy()">
+                      @change="EmitIfLiftExtruderBy($event)">
                     <div class="input-group-append">
                       <span class="input-group-text">mm</span>
                     </div>
                   </div>
                 </div>
               </div>
+
+              <!-- Advanced settngs: sensitivity slider -->
               <div class="form-group sensitivity my-4">
                 <div class="form-label text-muted">How sensitive do you want the Detective to be on this printer?</div>
-                <div class="px-3 my-2">
+                <div class="my-2 input-wrapper sensitivity-slider" id="sensitivityInputWrapper">
                   <input id="sensitivity" name="detective_sensitivity" data-slider-id='sensitivity-slider' type="text"
                     data-slider-min="0.8" data-slider-max="1.2" data-slider-step="0.05" :data-slider-value="sensitivity" />
                 </div>
@@ -186,24 +193,113 @@ export default {
   data() {
     return {
       retractFilamentByInput: '0',
-      liftExtruderByInput: '0'
+      liftExtruderByInput: '0',
+      retractFilamentByTimeoutId: null, // Is used to make 1s pause before sending new value to API
+      liftExtruderByTimeoutId: null // Is used to make 1s pause before sending new value to API
     }
   },
 
   methods: {
-    EmitIfEtractFilamentBy: function() {
-      if (this.retractFilamentBy !== false) {
+    /**
+     * Clear loading indicator (useful in case of error)
+     * @param {String} inputName
+     * @param {any} value
+     */
+    clearAPICallAnimation: function(inputName, value) {
+      const elem = this.getElementByName(inputName, value)
+      if (elem) {
+        elem.classList.remove('loading')
+      }
+    },
+
+    /**
+     * Show animation of successful settings save
+     * @param {String} inputName
+     * @param {any} value
+     */
+    successfullySavedFeedback: function(inputName, value) {
+      const elem = this.getElementByName(inputName, value)
+
+      if (elem) {
+        elem.classList.remove('loading')
+        elem.classList.add('successfully-saved')
+        setTimeout(
+          () => elem.classList.remove('successfully-saved'),
+          800
+        )
+      }
+    },
+
+    /**
+     * Emit input change to parent block
+     */
+
+    emitPauseAndNotify: function(event, value) {
+      event.target.closest('.input-wrapper').classList.add('loading')
+      this.$emit('pauseAndNotifyChanged', value)
+    },
+
+    emitIsHotendHeaterOff: function(event) {
+      event.target.closest('.input-wrapper').classList.add('loading')
+      this.$emit('isHotendHeaterOffChanged', event.target.checked)
+    },
+    emitIsBedHeaterOff: function(event) {
+      event.target.closest('.input-wrapper').classList.add('loading')
+      this.$emit('isBedHeaterOffChanged', event.target.checked)
+    },
+
+    emitRetractFilamentBy: function(event) {
+      event.target.closest('.input-wrapper').classList.add('loading')
+
+      if (event.target.checked) {
         this.$emit('retractFilamentByChanged', this.retractFilamentByInput)
+      } else {
+        this.$emit('retractFilamentByChanged', false)
       }
     },
-    EmitIfLiftExtruderBy: function() {
-      if (this.liftExtruderBy !== false) {
+    EmitIfEtractFilamentBy: function(event) {
+      if (this.retractFilamentBy !== false) {
+        if (this.retractFilamentByTimeoutId) {
+          clearTimeout(this.retractFilamentByTimeoutId)
+        }
+
+        this.retractFilamentByTimeoutId = setTimeout(() => {
+          event.target.closest('.input-wrapper').classList.add('loading')
+          this.$emit('retractFilamentByChanged', this.retractFilamentByInput)
+        }, 1000)
+      }
+    },
+
+    emitLiftExtruderBy: function(event) {
+      event.target.closest('.input-wrapper').classList.add('loading')
+
+      if (event.target.checked) {
         this.$emit('liftExtruderByChanged', this.liftExtruderByInput)
+      } else {
+        this.$emit('liftExtruderByChanged', false)
       }
     },
-    emitSensitivityChange: function(data) {
-      this.$emit('sensitivityChanged', String(data.newValue))
+    EmitIfLiftExtruderBy: function(event) {
+      if (this.liftExtruderBy !== false) {
+        if (this.liftExtruderByTimeoutId) {
+          clearTimeout(this.liftExtruderByTimeoutId)
+        }
+
+        this.liftExtruderByTimeoutId = setTimeout(() => {
+          event.target.closest('.input-wrapper').classList.add('loading')
+          this.$emit('liftExtruderByChanged', this.liftExtruderByInput)
+        }, 1000)
+      }
     },
+
+    emitSensitivityChange: function(newValue) {
+      document.querySelector('.input-wrapper.sensitivity-slider').classList.add('loading')
+      this.$emit('sensitivityChanged', String(newValue))
+    },
+
+    /**
+     * Update hint under sensitivity slider
+     */
     updateSensitivityHint: function() {
       // Hide all hints
       const hints = document.querySelectorAll('.sensitivity div[class^=hint]');
@@ -220,11 +316,35 @@ export default {
       } else {
         document.querySelector('.sensitivity .hint-medium').style.display = 'block'
       }
-    }
+    },
+
+    /**
+     * Returns input element by gived name
+     * @param {String} elementId
+     * @returns {Object, null}
+     */
+    getElementByName: function(inputName, value) {
+      switch (inputName) {
+        case 'pauseAndNotify':
+          return value ? document.querySelector('#pauseAndNotifyTrue'): document.querySelector('#pauseAndNotifyFalse')
+        case 'isHotendHeaterOff':
+          return document.querySelector('#isHotendHeaterOff')
+        case 'isBedHeaterOff':
+          return document.querySelector('#isBedHeaterOff')
+        case 'retractFilamentBy':
+          return document.querySelector('#retractFilamentBy')
+        case 'liftExtruderBy':
+          return document.querySelector('#liftExtruderBy')
+        case 'sensitivity':
+          return document.querySelector('#sensitivityInputWrapper')
+        default:
+          return null
+      }
+    },
   },
 
   mounted() {
-    // Put values into inputs placed near checkboxes
+    // Put values into inputs (placed near checkboxes)
     if (this.retractFilamentBy !== false) {
       this.retractFilamentByInput = this.retractFilamentBy
     }
@@ -245,7 +365,7 @@ export default {
       }
     })
 
-    sensitivitySlider.on('change', this.emitSensitivityChange) // Emit new value to parent component
+    sensitivitySlider.on('slideStop', this.emitSensitivityChange) // Emit new value to parent component
     sensitivitySlider.on('change', this.updateSensitivityHint) // Update hint depending of selected value
     this.updateSensitivityHint() // Initial hits update (hide all except one)
   },
@@ -256,7 +376,10 @@ export default {
 @use "~main/theme"
 
 .form-label
-  font-size: 20px
+  font-size: 18px
+
+.card
+  overflow: visible
 
 .custom-control-label
   font-size: 16px
@@ -264,7 +387,34 @@ export default {
 .input-group.minimal-width input
   width: 4rem
 
-// .sensitivity .tooltip
-//   position: relative
-//   bottom: 10px !important
+// API call animation
+.input-wrapper
+  $indicatorSize: 16px
+
+  &:before
+    content: ""
+    background-size: $indicatorSize $indicatorSize
+    width: $indicatorSize
+    height: $indicatorSize
+    display: block
+    position: absolute
+    top: 2px
+    right: 0
+    margin: auto
+    z-index: 9
+  
+  &.sensitivity-slider:before
+    right: -#{$indicatorSize + 16px}
+  
+  &.loading
+    position: relative
+
+    &:before
+      background-image: url('/static/img/tail-spin.svg')
+
+  &.successfully-saved
+    position: relative
+    
+    &:before
+      background-image: url('/static/img/tick.svg')
 </style>
--- web/frontend/src/printers/PrinterSettingsPage.vue
+++ web/frontend/src/printers/PrinterSettingsPage.vue
@@ -1,15 +1,237 @@
 <template>
 <div class="row justify-content-center">
-  <div class="col-sm-12 col-lg-10">
+  <div class="col-sm-12 col-md-10 col-lg-8 settings-container">
+    <section class="settings">
+      <h2 class="section-title">Settings</h2>
+      <div class="form-group mb-4 mt-4">
+        <div class="form-label text-muted mb-2">Give your shiny new printer a name</div>
+        <form @submit.prevent="newPrinterName" id="printerNameForm">
+          <input
+            id="printerName"
+            type="text"
+            name="name"
+            v-model="printerName"
+            maxlength="200"
+            placeholder="My Awesome 3D Printer"
+            class="form-control field_required"
+            required="required">
+            <small class="text-muted">Click Enter to save new name</small>
+        </form>
+      </div>
+
+      <PrinterPreferences
+        ref="PrinterPreferences"
+
+        :pauseAndNotify="pauseAndNotify"
+        :isHotendHeaterOff="advancedSettings.isHotendHeaterOff"
+        :isBedHeaterOff="advancedSettings.isBedHeaterOff"
+        :retractFilamentBy="advancedSettings.retractFilamentBy"
+        :liftExtruderBy="advancedSettings.liftExtruderBy"
+        :sensitivity="advancedSettings.sensitivity"
+
+        @pauseAndNotifyChanged="pauseAndNotifyChanged"
+        @isHotendHeaterOffChanged="isHotendHeaterOffChanged"
+        @isBedHeaterOffChanged="isBedHeaterOffChanged"
+        @retractFilamentByChanged="retractFilamentByChanged"
+        @liftExtruderByChanged="liftExtruderByChanged"
+        @sensitivityChanged="sensitivityChanged"/>
+    </section>
+    <section class="danger mt-5">
+      <h2 class="section-title">Danger Zone</h2>
+      <div class="text-center mt-4">
+        <button
+          class="btn btn-outline-danger"
+          @click="deletePrinter"
+        >
+          Delete Printer
+        </button>
+      </div>
+    </section>
   </div>
 </div>
 </template>
 
 <script>
+import PrinterPreferences from '../printers/PrinterPreferences'
+
 export default {
+  components: {
+    PrinterPreferences
+  },
+
+  data() {
+    return {
+      printerName: 'Epson',
+      pauseAndNotify: true, // true, false (settings: When a potential failure is detected)
+      advancedSettings: {
+        isHotendHeaterOff: true, // true, false
+        isBedHeaterOff: false, // true, false
+        retractFilamentBy: false, // false or number (in string format) from 0 with 0.5 step
+        liftExtruderBy: '2.5', // false or number (in string format) from 0 with 0.5 step
+        sensitivity: '1', // number (in string format) from 0.8 to 1.2 with 0.05 step
+      }
+    }
+  },
+
+  methods: {
+    /**
+     * Show animation of successful settings save
+     * @param {String} inputName
+     */
+    successfullySavedFeedback(inputName) {
+      let elem
+
+      switch (inputName) {
+        case 'printerName':
+          elem = document.querySelector('#printerNameForm')
+          break
+        default:
+          return
+      }
+      
+      elem.classList.remove('loading')
+      elem.classList.add('successfully-saved')
+      setTimeout(
+        () => elem.classList.remove('successfully-saved'),
+        800
+      )
+    },
+
+    /**
+     * Show error alert if can not save settings
+     */
+    errorAlert() {
+      this.$swal('Error occured!', '<p class="text-center">Can not save your new settings, please try again.</p>', 'error')
+    },
+
+    /**
+     * Printer name is changed - call the API
+     */
+    newPrinterName() {
+      const newName = document.querySelector('#printerName').value
+      if (newName) {
+        // TODO: call API
+        document.querySelector('#printerNameForm').classList.add('loading')
+        setTimeout(() => {
+          this.successfullySavedFeedback('printerName')
+        }, 1000)
+      }
+    },
+
+    /**
+     * Settings change emits from child PrinterPreferences component
+     */
+
+    pauseAndNotifyChanged(value) {
+      this.pauseAndNotify = value
+      console.log(value)
+      // TODO: call API
+      setTimeout(() => {
+        this.$refs.PrinterPreferences.successfullySavedFeedback('pauseAndNotify', value)
+      }, 1000)
+    },
+    isHotendHeaterOffChanged(value) {
+      this.advancedSettings.isHotendHeaterOff = value
+      console.log(value)
+      // TODO: call API
+      setTimeout(() => {
+        this.$refs.PrinterPreferences.successfullySavedFeedback('isHotendHeaterOff', value)
+      }, 1000)
+    },
+    isBedHeaterOffChanged(value) {
+      this.advancedSettings.isBedHeaterOff = value
+      console.log(value)
+      // TODO: call API
+      setTimeout(() => {
+        // Example of error alert
+        this.$refs.PrinterPreferences.clearAPICallAnimation('isBedHeaterOff', value)
+        this.errorAlert()
+      }, 1000)
+    },
+    retractFilamentByChanged(value) {
+      this.advancedSettings.retractFilamentBy = value
+      console.log(value)
+      // TODO: call API
+      setTimeout(() => {
+        this.$refs.PrinterPreferences.successfullySavedFeedback('retractFilamentBy', value)
+      }, 1000)
+    },
+    liftExtruderByChanged(value) {
+      this.advancedSettings.liftExtruderBy = value
+      console.log(value)
+      // TODO: call API
+      setTimeout(() => {
+        this.$refs.PrinterPreferences.successfullySavedFeedback('liftExtruderBy', value)
+      }, 1000)
+    },
+    sensitivityChanged(value) {
+      this.advancedSettings.sensitivity = value
+      console.log(value)
+      // TODO: call API
+      setTimeout(() => {
+        this.$refs.PrinterPreferences.successfullySavedFeedback('sensitivity', value)
+      }, 1000)
+    },
+    deletePrinter() {
+      this.$swal.Confirm.fire().then((result) => {
+        if (result.value) {
+          console.log('Delete printer')
+          // TODO: delete printer
+        }
+      })
+    }
+  }
 }
 </script>
 
 <style lang="sass" scoped>
 @use "~main/theme"
+
+.settings-container
+  margin: 2em 0em
+  padding: 2em 3em
+  background: theme.$body-bg
+  -webkit-box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.3) !important
+  box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.3) !important
+  border: none !important
+
+.section-title
+  padding-bottom: 5px
+  border-bottom: 1px solid theme.$white
+
+.form-label
+  font-size: 18px
+
+section.danger
+  .section-title
+    color: theme.$danger
+    border-color: theme.$danger
+
+// API call animation
+#printerNameForm
+  $indicatorSize: 16px
+
+  &:before
+    content: ""
+    background-size: $indicatorSize $indicatorSize
+    width: $indicatorSize
+    height: $indicatorSize
+    display: block
+    position: absolute
+    top: 10px
+    right: -#{$indicatorSize + 16px}
+    margin: auto
+    z-index: 9
+
+  &.loading
+    position: relative
+
+    &:before
+      background-image: url('/static/img/tail-spin.svg')
+
+  &.successfully-saved
+    position: relative
+    
+    &:before
+      background-image: url('/static/img/tick.svg')
 </style>

62b46bc92e4047a6634833790489136175adce53
Kenneth Jiang <kenneth.jiang@gmail.com>
2021-03-10 10:48:54 -0800

    Remove unnecessary db query to speed up tunnling

--- web/frontend/src/octoprint_tunnel/OctoPrintTunnelPage.vue
+++ web/frontend/src/octoprint_tunnel/OctoPrintTunnelPage.vue
@@ -5,8 +5,9 @@
     </pull-to-reveal>
 
     <div v-if="!isPro" class="floating-panel text-center pb-2">
-      <span class="text-muted">Month-To-Date Usage/Free Limit(<a href="https://www.thespaghettidetective.com/docs/octoprint-tunneling/#is-octoprint-tunneling-free-to-all-users">?</a>):</span> <span :class="usageClass">{{ usageMTD }}/{{ humanizedUsageCap }}</span>
-      <div v-if="overage">Your month-to-date tunneling usage is over the Free plan limit. Upgrade to the Pro plan to <a type="button" class="btn btn-sm btn-primary" href="/ent/pricing/">Get Unlimited Usage</a></div>
+      <div class="text-muted">Month-To-Date Usage/Free Limit(<a href="https://www.thespaghettidetective.com/docs/octoprint-tunneling/#is-octoprint-tunneling-free-to-all-users">?</a>)</div>
+      <div :class="usageClass">{{ usageMTD }}/{{ humanizedUsageCap }}</div>
+      <div v-if="overage">Your month-to-date tunneling usage is over the Free plan limit. <a type="button" class="btn btn-sm btn-primary" href="/ent/pricing/">Get Unlimited Tunneling</a></div>
     </div>
     <div>
       <iframe v-if="printerId" :src="iframeUrl()" class="tunnel-iframe"></iframe>
@@ -16,6 +17,7 @@
 
 <script>
 import axios from 'axios'
+import split from 'lodash/split'
 import filesize from 'filesize'
 import urls from '@lib/server_urls'
 import PullToReveal from '@common/PullToReveal.vue'
@@ -28,21 +30,12 @@ export default {
     PullToReveal,
     Navbar,
   },
-
-  props: {
-    printerId: {
-      type: Number
-    },
-    isPro: {
-      type: Boolean
-    },
-    usageCap: {
-      type: Number
-    },
-  },
   data: function() {
     return {
       bytesMTD: null,
+      usageCap: null,
+      isPro: true,
+      printerId: null,
     }
   },
 
@@ -65,9 +58,15 @@ export default {
     },
   },
 
+  created() {
+    this.isPro = JSON.parse(document.querySelector('#user-json').text).is_pro
+    this.printerId = split(window.location.pathname, '/').slice(-2, -1).pop()
+    this.usageCap = JSON.parse(document.querySelector('#settings-json').text).OCTOPRINT_TUNNEL_CAP
+  },
+
   mounted() {
     this.$swal.DismissableToast({
-        html: '<div class="p-1">It may take long time for OctoPrint page to load as it is securely tunneled via The Spaghetti Detective server.</div><div class="p-1"><a target="_blank" href="https://www.thespaghettidetective.com/docs/octoprint-tunneling/#is-octoprint-tunneling-secure">Learn more about OctoPrint Tunneling\'s security and page load speed. <i class="fas fa-external-link-alt"></i></a></div>',
+        html: '<h4 class="text-center p-2"><img style="height: 1.1em;margin-right: 0.75em;" src="/static/img/octoprint-tunnel.png" />OctoPrint Secure Tunnel</h4><div class="p-1">It may take long time for OctoPrint page to load as it is securely tunneled via The Spaghetti Detective server.</div><div class="p-1"><a target="_blank" href="https://www.thespaghettidetective.com/docs/octoprint-tunneling/#is-octoprint-tunneling-secure">Learn more about OctoPrint Tunneling\'s security and page load speed. <i class="fas fa-external-link-alt"></i></a></div>',
         customClass: {
           container: 'dark-backdrop',
         },

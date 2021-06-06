41644988ac1d188d9e832aa754a79fadfe84d677
Kenneth Jiang <kenneth.jiang@gmail.com>
2021-06-06 15:27:28 -0700

    jetson script

---
+++ scripts/install_on_jetson.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+sudo apt install -y python python-pip curl libffi-dev python-openssl libssl-dev zlib1g-dev gcc g++ make
+sudo python -m pip install --upgrade pip setuptools wheel
+sudo python -m pip install docker-compose
+
+cat <<EOT >docker-compose.override.yml
+version: '2.4'
+
+services:
+  ml_api:
+    runtime: nvidia
+    environment:
+        HAS_GPU: 'True'
+EOT

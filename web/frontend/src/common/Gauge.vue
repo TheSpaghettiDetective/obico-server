07cdf99d9b2a46c428f6fc238b2364080685e2f3
Dmitry Puzyrev <puzyrev.dmitry@gmail.com>
2021-06-11 21:35:53 +0300

    Add dynamic colors change to gauge canvas

--- web/frontend/src/common/Gauge.vue
+++ web/frontend/src/common/Gauge.vue
@@ -2,7 +2,7 @@
   <div class="tsd-gauge-container">
     <span id="title" :style="{color: titleColor}">{{ titleText }}</span>
     <div class="tsd-gauge">
-      <radial-gauge :value="value" :options="options"></radial-gauge>
+      <radial-gauge :value="value" :options="computedOptions"></radial-gauge>
     </div>
     <hr />
   </div>
@@ -34,49 +34,54 @@ export default {
     options: {
       // https://canvas-gauges.com/documentation/user-guide/configuration
       type: Object,
-      default: function () {
-        const inactiveColor = {
-          highlight1: theme.value === Themes.Light ? '#929292' : '#8395a7',
-          highlight2: theme.value === Themes.Light ? '#b7b7b7' : '#a8bacc',
-          highlight3: theme.value === Themes.Light ? '#7b7b7b' : '#6c7e90',
-          needle: theme.value === Themes.Light ? '#2d3e4f' : '#ffffff',
-        }
-        
-        return {
-          valueDec: 0,
-          valueInt: 0,
-          width: 240,
-          height: 240,
-          units: false,
-          box: false,
-          minValue: 0,
-          maxValue: 100,
-          majorTicks: ['', '', '', ''],
-          minorTicks: 4,
-          highlights: [
-            { from: 0, to: 33, color: this.isWatching ? '#5cb85c' : inactiveColor.highlight1 },
-            { from: 33, to: 67, color: this.isWatching ? '#f0ad4e' : inactiveColor.highlight2 },
-            { from: 67, to: 100, color: this.isWatching ? '#d9534f' : inactiveColor.highlight3 }
-          ],
-          colorPlate: 'rgba(255,255,255,.0)',
-          colorTitle: '#5cb85c',
-          colorStrokeTicks: '#EBEBEB',
-          colorNeedleEnd: this.isWatching ? 'rgba(255, 160, 122, .9)' : inactiveColor.needle,
-          colorNeedle: this.isWatching ? 'rgba(240, 128, 128, 1)' : inactiveColor.needle,
-          colorNeedleShadowUp: this.isWatching ? 'rgba(2,255,255,0.2)' : inactiveColor.needle,
-          valueBox: false,
-          animationRule: 'bounce',
-          animationDuration: 500,
-          animatedValue: true,
-          startAngle: 90,
-          ticksAngle: 180,
-          borders: false
-        }
-      }
+      default: null
     }
   },
 
   computed: {
+    computedOptions() {
+      if (this.options !== null) {
+        return this.options 
+      }
+      
+      const inactiveColor = {
+        highlight1: theme.value === Themes.Light ? '#929292' : '#8395a7',
+        highlight2: theme.value === Themes.Light ? '#b7b7b7' : '#a8bacc',
+        highlight3: theme.value === Themes.Light ? '#7b7b7b' : '#6c7e90',
+        needle: theme.value === Themes.Light ? '#2d3e4f' : '#ffffff',
+      }
+      
+      return {
+        valueDec: 0,
+        valueInt: 0,
+        width: 240,
+        height: 240,
+        units: false,
+        box: false,
+        minValue: 0,
+        maxValue: 100,
+        majorTicks: ['', '', '', ''],
+        minorTicks: 4,
+        highlights: [
+          { from: 0, to: 33, color: this.isWatching ? '#5cb85c' : inactiveColor.highlight1 },
+          { from: 33, to: 67, color: this.isWatching ? '#f0ad4e' : inactiveColor.highlight2 },
+          { from: 67, to: 100, color: this.isWatching ? '#d9534f' : inactiveColor.highlight3 }
+        ],
+        colorPlate: 'rgba(255,255,255,.0)',
+        colorTitle: '#5cb85c',
+        colorStrokeTicks: '#EBEBEB',
+        colorNeedleEnd: this.isWatching ? 'rgba(255, 160, 122, .9)' : inactiveColor.needle,
+        colorNeedle: this.isWatching ? 'rgba(240, 128, 128, 1)' : inactiveColor.needle,
+        colorNeedleShadowUp: this.isWatching ? 'rgba(2,255,255,0.2)' : inactiveColor.needle,
+        valueBox: false,
+        animationRule: 'bounce',
+        animationDuration: 500,
+        animatedValue: true,
+        startAngle: 90,
+        ticksAngle: 180,
+        borders: false
+      }
+    },
     value() {
       return this.normalizedP*100
     },

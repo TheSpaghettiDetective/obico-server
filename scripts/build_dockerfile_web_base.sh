59b928b341962c1fee604f54c72451ae9891de98
encetamasb <encetamasb@gmail.com>
2021-07-05 15:10:10 +0200

    Fixed path to dockerfile

--- scripts/build_dockerfile_web_base.sh
+++ scripts/build_dockerfile_web_base.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-docker buildx build web --push --platform linux/arm64/v8,linux/amd64 -f Dockerfile.base -t thespaghettidetective/web:base-$1
+docker buildx build web --push --platform linux/arm64/v8,linux/amd64 -f web/Dockerfile.base -t thespaghettidetective/web:base-$1

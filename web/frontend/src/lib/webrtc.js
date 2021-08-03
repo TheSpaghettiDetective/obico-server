00ffe82601f12f1199e51811919c8ff55b557062
encetamasb <encetamasb@gmail.com>
2021-08-03 15:06:24 +0200

    Fixed "Invalid Peerconnection": stop/start bitrate logger interval properly

--- web/frontend/src/lib/webrtc.js
+++ web/frontend/src/lib/webrtc.js
@@ -13,6 +13,7 @@ export default function WebRTCConnection(videoEnabled) {
     streamId: undefined,
     streaming: undefined,
     videoEnabled: videoEnabled ?? false,
+    bitrateInterval: null,
 
     openForShareToken(shareToken) {
       self.connect(
@@ -93,9 +94,6 @@ export default function WebRTCConnection(videoEnabled) {
                     }
                   }
                 })
-                setInterval(function() {
-                    console.log(pluginHandle.getBitrate())
-                }, 5000)
               },
               error: function (error) {
                 Janus.error('  -- Error attaching plugin... ', error)
@@ -201,9 +199,25 @@ export default function WebRTCConnection(videoEnabled) {
       }
       const body = { 'request': 'watch', offer_video: self.videoEnabled, id: parseInt(self.streamId) }
       self.streaming.send({ 'message': body })
+
+      if (self.bitrateInterval) {
+        clearInterval(self.bitrateInterval)
+        self.bitrateInterval = null
+      }
+
+      self.bitrateInterval = setInterval(function() {
+        if (self.streaming) {
+          console.log(self.streaming.getBitrate())
+        }
+      }, 5000)
     },
     stopStream() {
-    if (!self.channelOpen()) {
+      if (self.bitrateInterval) {
+        clearInterval(self.bitrateInterval)
+        self.bitrateInterval = null
+      }
+
+      if (!self.channelOpen()) {
         return
       }
       const body = { 'request': 'stop' }

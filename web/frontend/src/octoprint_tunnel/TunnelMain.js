e04d405291f9395df90102a729b811b2778fe725
Dmitry Puzyrev <puzyrev.dmitry@gmail.com>
2021-08-26 19:35:45 +0300

    Connect bootstrap-vue

--- web/frontend/src/octoprint_tunnel/TunnelMain.js
+++ web/frontend/src/octoprint_tunnel/TunnelMain.js
@@ -4,10 +4,12 @@ import VueSwal from 'common/VueSwal'
 import OctoPrintTunnelPage from './OctoPrintTunnelPage.vue'
 import setupSentry from '@lib/sentry'
 import { initTheme } from '@main/themes'
+import { BootstrapVue } from 'bootstrap-vue'
 
 initTheme()
 setupSentry(Vue)
 Vue.use(VueSwal)
+Vue.use(BootstrapVue)
 
 new Vue({
   components: { OctoPrintTunnelPage }
--- web/frontend/src/print_shot_feedback/main.js
+++ web/frontend/src/print_shot_feedback/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import VueSwal from 'common/VueSwal'
+import { BootstrapVue } from 'bootstrap-vue'
 import setupSentry from '@lib/sentry'
 import { initTheme } from '@main/themes'
 
 initTheme()
 setupSentry(Vue)
 Vue.use(VueSwal)
+Vue.use(BootstrapVue)
 
 import App from './PrintShotFeedbackApp.vue'
 

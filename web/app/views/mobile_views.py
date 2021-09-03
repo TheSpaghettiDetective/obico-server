95f2685dc4e33b9382ba54ef463c4b258f1e12c2
encetamasb <encetamasb@gmail.com>
2021-09-03 11:38:15 +0200

    Extended access token handler with apple provider

--- web/app/views/mobile_views.py
+++ web/app/views/mobile_views.py
@@ -11,6 +11,7 @@ from allauth.socialaccount.providers.base import ProviderException
 
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
+from allauth.socialaccount.providers.apple.views import AppleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.views import (
     OAuth2CallbackView,
 )
@@ -50,8 +51,18 @@ def fetch_session(request):
 def oauth_callback(request, *args, **kwargs):
     try:
         is_google = request.GET['provider'] == 'google'
+        is_facebook = request.GET['provider'] == 'facebook'
+        is_apple = request.GET['provider'] == 'apple'
 
-        adapter = GoogleOAuth2Adapter(request) if is_google else FacebookOAuth2Adapter(request)
+        if is_google:
+            adapter = GoogleOAuth2Adapter(request)
+        elif is_facebook:
+            adapter = FacebookOAuth2Adapter(request)
+        elif is_apple:
+            adapter = AppleOAuth2Adapter(request)
+        else:
+            raise Exception("Unsupported provider")
+
         provider = adapter.get_provider()
         app = adapter.get_provider().get_app(request)
 
@@ -59,25 +70,35 @@ def oauth_callback(request, *args, **kwargs):
             if is_google:
                 callback_url = adapter.get_callback_url(request, app)
                 scope = provider.get_scope(request)
-                client = OAuth2Client(request, app.client_id, app.secret,
-                                        adapter.access_token_method,
-                                        adapter.access_token_url,
-                                        callback_url,
-                                        scope,
-                                        scope_delimiter=adapter.scope_delimiter,
-                                        headers=adapter.headers,
-                                        basic_auth=adapter.basic_auth)
+                client = OAuth2Client(
+                    request,
+                    app.client_id,
+                    app.secret,
+                    adapter.access_token_method,
+                    adapter.access_token_url,
+                    callback_url,
+                    scope,
+                    scope_delimiter=adapter.scope_delimiter,
+                    headers=adapter.headers,
+                    basic_auth=adapter.basic_auth)
                 access_token = client.get_access_token(request.GET['code'])
                 token = adapter.parse_token(access_token)
-            else:
+            elif is_facebook:
                 access_token = request.GET['code']
                 token = adapter.parse_token({'access_token': access_token, 'token_type': 'bearer', 'expires_in': 5179237})      # hard-coded properties to make allauth happy
+            elif is_apple:
+                access_token = request.GET['code']
+                id_token = request.GET['id_token']
+                refresh_token = request.GET.get('refresh_token', '')
+                token = adapter.parse_token({
+                    'access_token': access_token, 'id_token': id_token, 'refresh_token': refresh_token})
 
             token.app = app
-            login = adapter.complete_login(request,
-                                                app,
-                                                token,
-                                                response=access_token)
+            login = adapter.complete_login(
+                request,
+                app,
+                token,
+                response=access_token)
             login.token = token
             return complete_social_login(request, login)
         except (PermissionDenied,

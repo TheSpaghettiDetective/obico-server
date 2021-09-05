1b57711ce6de248c1e9691077cdb47a158c6e55d
Kenneth Jiang <kenneth.jiang@gmail.com>
2021-09-05 11:17:07 -0700

    Add router for path flexibility

--- web/frontend/src/users/main.js
+++ web/frontend/src/users/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import VueSwal from 'common/VueSwal'
 import setupSentry from '@lib/sentry'
 import { BootstrapVue } from 'bootstrap-vue'
@@ -6,10 +7,21 @@ import { initTheme } from '@main/themes'
 
 initTheme()
 setupSentry(Vue)
+Vue.use(VueRouter)
 Vue.use(VueSwal)
 Vue.use(BootstrapVue)
 
 import UserPreferencesPage from './UserPreferencesPage.vue'
+
+const router = new VueRouter({
+    routes: [
+      {
+        path: '/',
+        component: UserPreferencesPage,
+      },
+    ]
+  })
+
 new Vue({
-  components: { UserPreferencesPage }
+  router
 }).$mount('#user-preferences-mount')

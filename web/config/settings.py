11a24d8ba4dab6b61d33f49d605a60a655e1b470
Kenneth Jiang <kenneth.jiang@gmail.com>
2021-11-07 20:31:23 -0800

    Remove unused code

--- web/config/settings.py
+++ web/config/settings.py
@@ -313,9 +313,7 @@ PUSHOVER_APP_TOKEN = os.environ.get('PUSHOVER_APP_TOKEN')
 SLACK_CLIENT_ID = None
 
 OCTOPRINT_TUNNEL_CAP = int(os.environ.get('OCTOPRINT_TUNNEL_CAP', '1099511627776'))  # 1TB by default
-OCTOPRINT_TUNNEL_SUBDOMAIN_FMT = '{subdomain_code}.tunnels.{site.domain}'
 OCTOPRINT_TUNNEL_SUBDOMAIN_RE = re.compile(r'^(\w+)\.tunnels.*$')
-
 if os.environ.get('OCTOPRINT_TUNNEL_PORT_RANGE'):
     OCTOPRINT_TUNNEL_PORT_RANGE = tuple(
         map(int, os.environ.get('OCTOPRINT_TUNNEL_PORT_RANGE').split('-')[:2])

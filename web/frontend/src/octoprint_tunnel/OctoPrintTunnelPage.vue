cc61feb70b6f9f247825cd3973e61c0d46bbf8cf
Dmitry Puzyrev <puzyrev.dmitry@gmail.com>
2021-11-26 14:40:15 +0300

    Show correct usage on first tunneling load

--- web/frontend/src/octoprint_tunnel/OctoPrintTunnelPage.vue
+++ web/frontend/src/octoprint_tunnel/OctoPrintTunnelPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-if="!isPro" class="floating-panel text-center pb-2">
+    <div v-if="!isPro && usageFetched" class="floating-panel text-center pb-2">
       <div class="text-muted">Month-To-Date Usage/Free Limit(<a href="https://www.thespaghettidetective.com/docs/octoprint-tunneling/#is-octoprint-tunneling-free-to-all-users">?</a>)</div>
       <div :class="usageClass">{{ usageMTD }}/{{ humanizedUsageCap }}</div>
       <div v-if="overage">Your month-to-date tunneling usage is over the Free plan limit. <a type="button" class="btn btn-sm btn-primary" href="/ent_pub/pricing/">Get Unlimited Tunneling</a></div>
@@ -28,6 +28,7 @@ export default {
     return {
       bytesMTD: null,
       usageCap: null,
+      usageFetched: false,
       isPro: true,
       printerId: null,
     }
@@ -91,16 +92,19 @@ export default {
     }
 
     const self = this
-    const fetchUsage = () => {
+    const fetchUsage = (firstFetch = false) => {
       axios
         .get(urls.tunnelUsage())
         .then((resp) => {
           self.bytesMTD = resp.data.total
           self.usageCap = resp.data.monthly_cap
+          if (firstFetch) {
+            self.usageFetched = true
+          }
         })
     }
     setInterval(fetchUsage, 15*1000)
-    setTimeout(fetchUsage, 4000)
+    setTimeout(() => { fetchUsage(true) }, 4000)
   },
 
   methods: {

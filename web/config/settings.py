8285329487ba5ce99b85da626d9535b6b18d1600
encetamasb <encetamasb@gmail.com>
2022-01-24 17:38:46 +0100

    Build current Site object from env vars and use that

--- web/config/settings.py
+++ web/config/settings.py
@@ -40,8 +40,10 @@ SESSION_COOKIE_AGE = 60 * 60 * 24 * 60  # User login session is 2 months
 SESSION_SAVE_EVERY_REQUEST = True
 SESSION_COOKIE_NAME = 'tsd_sessionid'
 
-if os.environ.get('SITE_DOMAIN'):
-    SESSION_COOKIE_DOMAIN = os.environ.get('SITE_DOMAIN')
+SITE_DOMAIN = os.environ.get('SITE_DOMAIN')
+SITE_NAME = os.environ.get('SITE_NAME', 'The Spaghetti Detective')
+if SITE_DOMAIN:
+    SESSION_COOKIE_DOMAIN = SITE_DOMAIN
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = get_bool('DEBUG', False)

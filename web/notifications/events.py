e8f2722bb63df2f778e3d35a2b508df9ffe26f7d
Kenneth Jiang <kenneth.jiang@gmail.com>
2022-04-06 20:34:22 -0700

    More explicit way to specify what events are considered 'other print events'

--- web/notifications/events.py
+++ web/notifications/events.py
@@ -1,30 +1,19 @@
 from typing import NewType, Dict
 
 PrinterEvent = NewType('PrinterEvent', str)
-__PRINTER_EVENTS: Dict[str, PrinterEvent] = {}
 
+PrintStarted = PrinterEvent('PrintStarted',)
+PrintFailed = PrinterEvent('PrintFailed')
+PrintDone = PrinterEvent('PrintDone')
+PrintCancelled = PrinterEvent('PrintCancelled')
+PrintPaused = PrinterEvent('PrintPaused')
+PrintResumed = PrinterEvent('PrintResumed')
 
-def _printer_event(name: str, store: Dict = __PRINTER_EVENTS) -> PrinterEvent:
-    event = PrinterEvent(name)
-    assert name not in store
-    store[name] = event
-    return event
+PrintProgress = PrinterEvent('PrintProgress')
 
+FilamentChange = PrinterEvent('FilamentChange')
 
-def is_supported(name: str, store: Dict = __PRINTER_EVENTS) -> bool:
-    return name in store
+HeaterCooledDown = PrinterEvent('HeaterCooledDown')
+HeaterTargetReached = PrinterEvent('HeaterTargetReached')
 
-
-PrintStarted = _printer_event('PrintStarted',)
-PrintFailed = _printer_event('PrintFailed')
-PrintDone = _printer_event('PrintDone')
-PrintCancelled = _printer_event('PrintCancelled')
-PrintPaused = _printer_event('PrintPaused')
-PrintResumed = _printer_event('PrintResumed')
-
-PrintProgress = _printer_event('PrintProgress')
-
-FilamentChange = _printer_event('FilamentChange')
-
-HeaterCooledDown = _printer_event('HeaterCooledDown')
-HeaterTargetReached = _printer_event('HeaterTargetReached')
+OTHER_PRINT_EVENTS = [PrintStarted, PrintPaused, PrintResumed]

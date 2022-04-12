f5c9e11be513884efd3ace9e9c9cc198fffe412d
Kenneth Jiang <kenneth.jiang@gmail.com>
2022-04-11 19:53:53 -0700

    Migrate SMS to notification v2

--- web/app/migrations/0064_notificationsetting.py
+++ web/app/migrations/0064_notificationsetting.py
@@ -48,35 +48,42 @@ class Migration(migrations.Migration):
         migrations.RunSQL(
             sql="""
                 insert into app_notificationsetting(name, user_id, config_json, enabled, notify_on_failure_alert, notify_on_print_done, notify_on_print_cancelled, notify_on_filament_change, notify_on_other_print_events, notify_on_heater_status, created_at, updated_at)
-                select 'discord', id, ' {"webhook_url": "' || discord_webhook || '"}', 't', 't', notify_on_done and print_notification_by_discord, notify_on_canceled and print_notification_by_discord, notify_on_filament_change_req and print_notification_by_discord, 'f', 'f', now(), now() from app_user where discord_webhook is not null;
+                select 'discord', id, '{"webhook_url": "' || discord_webhook || '"}', 't', 't', notify_on_done and print_notification_by_discord, notify_on_canceled and print_notification_by_discord, notify_on_filament_change_req and print_notification_by_discord, 'f', 'f', now(), now() from app_user where discord_webhook is not null;
             """,
             reverse_sql=migrations.RunSQL.noop,
         ),
         migrations.RunSQL(
             sql="""
                 insert into app_notificationsetting(name, user_id, config_json, enabled, notify_on_failure_alert, notify_on_print_done, notify_on_print_cancelled, notify_on_filament_change, notify_on_other_print_events, notify_on_heater_status, created_at, updated_at)
-                select 'telegram', id, ' {"chat_id": "' || telegram_chat_id || '"}', 't', 't', notify_on_done and print_notification_by_telegram, notify_on_canceled and print_notification_by_telegram, notify_on_filament_change_req and print_notification_by_telegram, 'f', 'f', now(), now() from app_user where telegram_chat_id is not null;
+                select 'telegram', id, '{"chat_id": "' || telegram_chat_id || '"}', 't', 't', notify_on_done and print_notification_by_telegram, notify_on_canceled and print_notification_by_telegram, notify_on_filament_change_req and print_notification_by_telegram, 'f', 'f', now(), now() from app_user where telegram_chat_id is not null;
             """,
             reverse_sql=migrations.RunSQL.noop,
         ),
         migrations.RunSQL(
             sql="""
                 insert into app_notificationsetting(name, user_id, config_json, enabled, created_at, updated_at)
-                select 'slack', id, ' {"access_token": "' || slack_access_token || '"}', 't', now(), now() from app_user where slack_access_token is not null;
+                select 'slack', id, '{"access_token": "' || slack_access_token || '"}', 't', now(), now() from app_user where slack_access_token is not null;
             """,
             reverse_sql=migrations.RunSQL.noop,
         ),
         migrations.RunSQL(
             sql="""
                 insert into app_notificationsetting(name, user_id, config_json, enabled, notify_on_failure_alert, notify_on_print_done, notify_on_print_cancelled, notify_on_filament_change, notify_on_other_print_events, notify_on_heater_status, created_at, updated_at)
-                select 'pushbullet', id, ' {"access_token": "' || pushbullet_access_token || '"}', 't', 't', notify_on_done and print_notification_by_pushbullet, notify_on_canceled and print_notification_by_pushbullet, notify_on_filament_change_req and print_notification_by_pushbullet, 'f', 'f', now(), now() from app_user where  pushbullet_access_token is not null;
+                select 'pushbullet', id, '{"access_token": "' || pushbullet_access_token || '"}', 't', 't', notify_on_done and print_notification_by_pushbullet, notify_on_canceled and print_notification_by_pushbullet, notify_on_filament_change_req and print_notification_by_pushbullet, 'f', 'f', now(), now() from app_user where  pushbullet_access_token is not null;
             """,
             reverse_sql=migrations.RunSQL.noop,
         ),
         migrations.RunSQL(
             sql="""
                 insert into app_notificationsetting(name, user_id, config_json, enabled, notify_on_failure_alert, notify_on_print_done, notify_on_print_cancelled, notify_on_filament_change, notify_on_other_print_events, notify_on_heater_status, created_at, updated_at)
-                select 'pushover', id, ' {"user_key": "' || pushover_user_token || '"}', 't', 't', notify_on_done and print_notification_by_pushover, notify_on_canceled and print_notification_by_pushover, notify_on_filament_change_req and print_notification_by_pushover, 'f', 'f', now(), now() from app_user where  pushover_user_token is not null;
+                select 'pushover', id, '{"user_key": "' || pushover_user_token || '"}', 't', 't', notify_on_done and print_notification_by_pushover, notify_on_canceled and print_notification_by_pushover, notify_on_filament_change_req and print_notification_by_pushover, 'f', 'f', now(), now() from app_user where  pushover_user_token is not null;
+            """,
+            reverse_sql=migrations.RunSQL.noop,
+        ),
+        migrations.RunSQL(
+            sql="""
+                insert into app_notificationsetting(name, user_id, config_json, enabled, notify_on_failure_alert, notify_on_print_done, notify_on_print_cancelled, notify_on_filament_change, notify_on_other_print_events, notify_on_heater_status, created_at, updated_at)
+                select 'twilio', id, '{"phone_country_code": "' || phone_country_code || '", "phone_number": "' || phone_number || '"}', 't', alert_by_sms, 'f', 'f', 'f', 'f', 'f', now(), now() from app_user where  phone_number is not null and phone_country_code is not null;
             """,
             reverse_sql=migrations.RunSQL.noop,
         ),

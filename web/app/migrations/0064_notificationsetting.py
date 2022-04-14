7d7fa451eccfd7e0f76fcbc34d830183c39d5299
Kenneth Jiang <kenneth.jiang@gmail.com>
2022-04-13 21:06:09 -0700

    Bug in migration. Raw SQL doesn't fill deafults

--- web/app/migrations/0064_notificationsetting.py
+++ web/app/migrations/0064_notificationsetting.py
@@ -19,8 +19,8 @@ def forwards(apps, schema_editor):
                 select 'telegram', id, '{"chat_id": "' || telegram_chat_id || '"}', 't', 't', notify_on_done and print_notification_by_telegram, notify_on_canceled and print_notification_by_telegram, notify_on_filament_change_req and print_notification_by_telegram, 'f', 'f', now(), now() from app_user where telegram_chat_id is not null;
             """)
         schema_editor.execute("""
-                insert into app_notificationsetting(name, user_id, config_json, enabled, created_at, updated_at)
-                select 'slack', id, '{"access_token": "' || slack_access_token || '"}', 't', now(), now() from app_user where slack_access_token is not null;
+                insert into app_notificationsetting(name, user_id, config_json,  enabled, notify_on_failure_alert, notify_on_print_done, notify_on_print_cancelled, notify_on_filament_change, notify_on_other_print_events, notify_on_heater_status, created_at, updated_at)
+                select 'slack', id, '{"access_token": "' || slack_access_token || '"}', 't', 't', 't', 'f', 't', 'f', 'f', now(), now() from app_user where slack_access_token is not null;
             """)
         schema_editor.execute("""
                 insert into app_notificationsetting(name, user_id, config_json, enabled, notify_on_failure_alert, notify_on_print_done, notify_on_print_cancelled, notify_on_filament_change, notify_on_other_print_events, notify_on_heater_status, created_at, updated_at)
@@ -50,7 +50,7 @@ def forwards(apps, schema_editor):
             """)
         schema_editor.execute("""
                 insert into app_notificationsetting(name, user_id, config_json, enabled, created_at, updated_at)
-                select 'slack', id, '{"access_token": "' || slack_access_token || '"}', 't', date(), date() from app_user where slack_access_token is not null;
+                select 'slack', id, '{"access_token": "' || slack_access_token || '"}', 't', 't', 't', 'f', 't', 'f', 'f', date(), date() from app_user where slack_access_token is not null;
             """)
         schema_editor.execute("""
                 insert into app_notificationsetting(name, user_id, config_json, enabled, notify_on_failure_alert, notify_on_print_done, notify_on_print_cancelled, notify_on_filament_change, notify_on_other_print_events, notify_on_heater_status, created_at, updated_at)

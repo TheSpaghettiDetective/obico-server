b837247b45e489594c4a220b9160444020325cad
Kenneth Jiang <kenneth.jiang@gmail.com>
2022-04-20 06:52:02 -0700

    Remove sentry configs that are not set up in TSD

--- ml_api/server.py
+++ ml_api/server.py
@@ -20,17 +20,6 @@ if environ.get('SENTRY_DSN'):
     sentry_sdk.init(
         dsn=environ.get('SENTRY_DSN'),
         integrations=[FlaskIntegration(), ],
-        # Set traces_sample_rate to 1.0 to capture 100%
-        # of transactions for performance monitoring.
-        # We recommend adjusting this value in production.
-        traces_sample_rate=1.0,
-
-        # By default the SDK will try to use the SENTRY_RELEASE
-        # environment variable, or infer a git commit
-        # SHA as release, however you may want to set
-        # something more human-readable.
-        # release="myapp@1.0.0",
-        release=environ.get('VERSION'),
     )
 
 app = flask.Flask(__name__)

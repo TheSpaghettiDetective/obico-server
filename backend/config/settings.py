5734c504fe4236ccb7bab1e17478cec7f1f46d03
Kenneth Jiang <kenneth.jiang@gmail.com>
2022-04-21 15:22:54 -0700

    Disable performance monitoring as it seems to cause a lot of overhead

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -282,12 +282,6 @@ if SENTRY_DSN:
             DjangoIntegration(),
             CeleryIntegration(),
         ],
-
-        # Set traces_sample_rate to 1.0 to capture 100%
-        # of transactions for performance monitoring.
-        # We recommend adjusting this value in production,
-        traces_sample_rate=1.0,
-
         # If you wish to associate users to errors (assuming you are using
         # django.contrib.auth) you may enable sending PII data.
         send_default_pii=True,

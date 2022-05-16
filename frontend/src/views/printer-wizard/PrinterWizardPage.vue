e18a973d4de5f99ef2a6bedabc10baa50bfd85a6
Kenneth Jiang <kenneth.jiang@gmail.com>
2022-05-15 22:12:04 -0700

    Help doc based on target platform

--- frontend/src/views/printer-wizard/PrinterWizardPage.vue
+++ frontend/src/views/printer-wizard/PrinterWizardPage.vue
@@ -502,14 +502,21 @@ export default {
       }, 5000)
     },
     showVerificationCodeHelpModal() {
-      this.$swal.fire({
-        title: 'Can\'t find the page to enter the 6-digit code?',
-        html: `<p>The 6-digit code needs to be entered in the Obico plugin in OctoPrint. There are a few reasons why you can't find this page:</p>
+      let html = `<p>The 6-digit code needs to be entered in the Obico plugin in OctoPrint. There are a few reasons why you can't find this page:</p>
         <p><ul>
         <li style="margin: 10px 0;">You don't have the plugin installed or you haven't restarted OctoPrint after installation. Click <a href="/printers/wizard/">here</a> to walk through the process again.</li>
         <li style="margin: 10px 0;">The installed plugin is on a version earlier than 1.5.0. You need to upgrade the plugin to <b>1.5.0</b> or later.</li>
         <li style="margin: 10px 0;">Still no dice? Check out the step-by-step <a target="_blank" href="https://www.obico.io/docs/user-guides/octoprint-plugin-setup/">set up guide</a>.</li>
-        </ul></p>`,
+        </ul></p>`
+
+      if (!this.targetOctoPrint) {
+        html = `<p>The 6-digit code needs to be entered to the <em>Obico for OctoPrint</em> installation script.</p>
+        <p>Check <a target="_blank" href="https://www.obico.io/docs/user-guides/klipper-setup/">this set up guide</a> for detailed instructions.</p>`
+      }
+
+      this.$swal.fire({
+        title: 'Can\'t find the page to enter the 6-digit code?',
+        html,
         customClass: {
           container: 'dark-backdrop',
         },

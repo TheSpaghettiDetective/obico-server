36228e781ffefdbc259abafd8125a4585c8e4baa
Kenneth Jiang <kenneth.jiang@gmail.com>
2022-06-02 05:55:39 -0700

    No reason to show download link when the video file is archived

--- frontend/src/components/prints/PrintCard.vue
+++ frontend/src/components/prints/PrintCard.vue
@@ -34,11 +34,11 @@
           <template #button-content>
             <i class="fas fa-ellipsis-v"></i>
           </template>
-          <b-dropdown-item v-if="this.print.video_url" :href="this.print.video_url" target="_blank">
+          <b-dropdown-item v-if="this.print.video_url && !print.video_archived_at" :href="this.print.video_url" target="_blank">
             <i class="fas fa-download"></i>Download Original Time-lapse
           </b-dropdown-item>
           <b-dropdown-item
-            v-if="this.print.tagged_video_url"
+            v-if="this.print.tagged_video_url && !print.video_archived_at"
             :href="this.print.tagged_video_url" target="_blank"
           >
             <i class="fas fa-download"></i>Download Detective Time-lapse

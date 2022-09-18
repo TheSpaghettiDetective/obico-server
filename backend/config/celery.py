0ac45373ac98b61dba344d9970050b3a03ee80f8
Kenneth Jiang <kenneth.jiang@gmail.com>
2022-09-18 14:10:35 -0700

    It looks like printer notifications have been sent to the wrong queue

--- backend/config/celery.py
+++ backend/config/celery.py
@@ -15,7 +15,8 @@ celery_app.conf.task_routes = {
     'app_ent.tasks.credit_dh_for_contribution': {'queue': 'realtime'},
     'app_ent.tasks.process_print_events_ent': {'queue': 'realtime'},
     'app_ent.tasks.setup_free_trial': {'queue': 'realtime'},
-    'notifications.tasks.send_print_notifications': {'queue': 'realtime'},
+    'notifications.tasks.send_printer_notifications': {'queue': 'realtime'},
+    'notifications.tasks.send_failure_alerts': {'queue': 'realtime'},
 }
 
 # Using a string here means the worker doesn't have to serialize

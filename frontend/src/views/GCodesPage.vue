f6dff3dc9c3cd2152db5e746fc37e42ac3fdac6a
dipuzyrev <puzyrev.dmitry@gmail.com>
2022-12-09 03:21:24 +0600

    Refine GCodesPage.vue code

--- frontend/src/views/GCodesPage.vue
+++ frontend/src/views/GCodesPage.vue
@@ -1,5 +1,7 @@
 <template>
   <layout>
+
+    <!-- Tob bar -->
     <template v-slot:topBarLeft>
       <search-input @input="updateSearch" class="search-input mr-3"></search-input>
     </template>
@@ -44,6 +46,7 @@
       </div>
     </template>
 
+    <!-- Page content -->
     <template v-slot:content>
       <b-container>
         <b-row>
@@ -65,7 +68,6 @@
               </div>
             </vue-dropzone>
 
-            <!-- GCodes list -->
             <div class="gcodes-wrapper">
               <div class="sorting-panel">
                 <div
@@ -115,6 +117,7 @@
               </div>
 
               <div class="gcode-items-wrapper" v-if="!loading">
+                <!-- Folders -->
                 <div v-if="!searchStateIsActive">
                   <div v-for="item in foldersToShow" :key="`folder_${item.id}`" class="item folder" @click="openFolder(item)">
                     <div class="item-info">
@@ -147,6 +150,7 @@
                   </div>
                 </div>
 
+                <!-- Gcodes -->
                 <div v-for="item in gcodesToShow" :key="`gcode_${item.id}`" class="item">
                   <div class="item-info">
                     <div class="filename">
@@ -189,14 +193,13 @@
                   </div>
                 </div>
 
+                <!-- Placeholders -->
                 <div v-if="!searchStateIsActive && !gcodesToShow.length && !foldersToShow.length" class="placeholder text-secondary">
                   <span>Nothing here yet</span>
                 </div>
-
                 <div v-if="searchStateIsActive && searchTimeoutId" class="placeholder">
                   <b-spinner />
                 </div>
-
                 <div v-if="searchStateIsActive && !searchTimeoutId && !gcodesToShow.length" class="placeholder text-secondary">
                   <span>Nothing found</span>
                 </div>
@@ -219,27 +222,26 @@ import { normalizedGcode, normalizedGcodeFolder } from '@src/lib/normalizers'
 import { user } from '@src/lib/page_context'
 import SearchInput from '@src/components/SearchInput.vue'
 
-const SEARCH_API_CALL_DELAY = 1000 // waiting time (ms) before asking server for search results
+// Waiting time (ms) before asking server for search results
+const SEARCH_API_CALL_DELAY = 1000
 
+// Sorting consts
 const SORTING_OPTIONS = {
   NAME: 1,
   SIZE: 2,
   CREATED: 3,
   LAST_PRINTED: 4,
 }
-
 const SORTING_OPTIONS_TITLE = {
   [SORTING_OPTIONS.NAME]: 'Name',
   [SORTING_OPTIONS.SIZE]: 'Size',
   [SORTING_OPTIONS.CREATED]: 'Created',
   [SORTING_OPTIONS.LAST_PRINTED]: 'Last Printed',
 }
-
 const SORTING_DIRECTIONS = {
   ASC: 1,
   DESC: -1,
 }
-
 const SORTING_DIRECTION_TITLE = {
   [SORTING_DIRECTIONS.ASC]: 'Ascending',
   [SORTING_DIRECTIONS.DESC]: 'Descending',
@@ -264,31 +266,45 @@ export default {
   data() {
     return {
       user: null,
-      searchStateIsActive: false,
-      searchResultGcodes: [],
-      searchTimeoutId: null,
+      loading: false,
+      parentFolder: null,
+      gcodes: [],
+      folders: [],
+
       activeSorting: SORTING_OPTIONS.CREATED,
       activeSortingDirection: SORTING_DIRECTIONS.DESC,
       sortingOptions: SORTING_OPTIONS,
       sortingTitle: SORTING_OPTIONS_TITLE,
       sortingDirectionTitle: SORTING_DIRECTION_TITLE,
       sortingDirections: SORTING_DIRECTIONS,
-      gcodes: [],
-      folders: [],
-      loading: false,
-      parentFolder: null,
+
+      searchStateIsActive: false,
+      searchResultGcodes: [],
+      searchTimeoutId: null,
     }
+  },
+
+  created() {
+    this.user = user()
+    this.parentFolder = this.$route.params.parentFolder || null
+    this.fetchFilesAndFolders()
+
+    this.$watch(
+      () => this.$route.params,
+      (toParams, previousParams) => {
+        this.parentFolder = toParams.parentFolder || null
+        this.fetchFilesAndFolders()
+      }
+    )
   },
 
   computed: {
     maxFilesize() {
       return this.user.is_pro ? 500 : 50 // MB
     },
-
     maxTotalFilesize() {
       return this.user.is_pro ? 50 : 1 // GB
     },
-
     dropzoneOptions() {
       return {
         withCredentials: true,
@@ -299,7 +315,6 @@ export default {
         headers: { 'X-CSRFToken': this.csrf },
       }
     },
-
     gcodesToShow() {
       let gcodes = this.gcodes
 
@@ -318,14 +333,12 @@ export default {
           gcodes.sort(function(a, b) {
             var filenameA = a.filename.toUpperCase() // ignore upper and lowercase
             var filenameB = b.filename.toUpperCase() // ignore upper and lowercase
-
             if (filenameA < filenameB) {
               return sortDirection === SORTING_DIRECTIONS.ASC ? -1 : 1
             }
             if (filenameA > filenameB) {
               return sortDirection === SORTING_DIRECTIONS.ASC ? 1 : -1
             }
-
             return 0
           })
           break
@@ -344,7 +357,6 @@ export default {
           gcodes.sort(function(a, b) {
             const uploadedA = a.created_at.unix()
             const uploadedB = b.created_at.unix()
-
             if (sortDirection === SORTING_DIRECTIONS.ASC) {
               return uploadedA - uploadedB
             } else {
@@ -357,7 +369,6 @@ export default {
           gcodes.sort(function(a, b) {
             const printedA = a.last_printed_at?.unix() || 0
             const printedB = b.last_printed_at?.unix() || 0
-
             if (sortDirection === SORTING_DIRECTIONS.ASC) {
               return printedA - printedB
             } else {
@@ -369,7 +380,6 @@ export default {
 
       return gcodes
     },
-
     foldersToShow() {
       let folders = this.folders
 
@@ -384,14 +394,12 @@ export default {
           folders.sort(function(a, b) {
             var nameA = a.name.toUpperCase() // ignore upper and lowercase
             var nameB = b.name.toUpperCase() // ignore upper and lowercase
-
             if (nameA < nameB) {
               return sortDirection === SORTING_DIRECTIONS.ASC ? -1 : 1
             }
             if (nameA > nameB) {
               return sortDirection === SORTING_DIRECTIONS.ASC ? 1 : -1
             }
-
             return 0
           })
           break
@@ -410,7 +418,6 @@ export default {
           folders.sort(function(a, b) {
             const createdA = a.created_at.unix()
             const createdB = b.created_at.unix()
-
             if (sortDirection === SORTING_DIRECTIONS.ASC) {
               return createdA - createdB
             } else {
@@ -419,26 +426,49 @@ export default {
           })
           break
       }
-
       return folders
     },
   },
 
-  created() {
-    this.user = user()
-    this.parentFolder = this.$route.params.parentFolder || null
-    this.fetchGCodes()
-
-    this.$watch(
-      () => this.$route.params,
-      (toParams, previousParams) => {
-        this.parentFolder = toParams.parentFolder || null
-        this.fetchGCodes()
-      }
-    )
-  },
-
   methods: {
+    async fetchFilesAndFolders() {
+      this.loading = true
+      this.gcodes = []
+      this.folders = []
+      let files
+      let folders
+
+      try {
+        files = await axios.get(urls.gcodeFiles({parentFolder: this.parentFolder}))
+        folders = await axios.get(urls.gcodeFolders({parentFolder: this.parentFolder}))
+      } catch (e) {
+        this.loading = false
+        console.error(e)
+      }
+
+      files = files?.data
+      folders = folders?.data
+
+      if (files?.count) {
+        this.gcodes = files.results.map(data => normalizedGcode(data))
+      }
+
+      if (folders?.count) {
+        folders = folders.results.map(data => normalizedGcodeFolder(data))
+        for (let folder in folders) {
+          try {
+            const numFiles = (await axios.get(urls.gcodeFiles({parentFolder: folders[folder].id}))).data.count || 0
+            const numFolders = (await axios.get(urls.gcodeFolders({parentFolder: folders[folder].id}))).data.count || 0
+            folders[folder].numItems = numFiles + numFolders
+          } catch (e) {
+            this.loading = false
+            console.error(e)
+          }
+        }
+        this.folders = folders
+      }
+      this.loading = false
+    },
     updateSearch(search) {
       clearTimeout(this.searchTimeoutId)
       this.searchTimeoutId = null
@@ -470,20 +500,103 @@ export default {
         this.searchTimeoutId = null
       }, SEARCH_API_CALL_DELAY);
     },
-
-
+    updateSorting(sortOption) {
+      if (this.activeSorting === sortOption) {
+        this.activeSortingDirection = -this.activeSortingDirection
+      } else {
+        this.activeSorting = sortOption
+        this.activeSortingDirection = SORTING_DIRECTIONS.ASC
+      }
+    },
     addParentFolderParam(file, xhr, formData) {
       formData.append('filename', file.name)
       if (this.parentFolder !== null) {
         formData.append('parent_folder', this.parentFolder)
       }
     },
-    openFolder(folder) {
-      // Prevent navigation if main user action was to click dropdown menu item
-      if (document.querySelector('.swal2-container')) {
-        return false
-      }
-      this.$router.push('/g_code_files/' + folder.id + '/')
+    gcodeUploadSuccess() {
+      this.$refs.gcodesDropzone.removeAllFiles()
+      this.gcodes = []
+      this.fetchFilesAndFolders()
+    },
+    gcodeUploadError(file, message) {
+      this.$swal.Reject.fire({
+        html: `<p class="text-center">${message}</p>`})
+    },
+    removeItem(id) {
+      axios
+        .delete(urls.gcode(id), )
+        .then(() => {
+          for (let i = 0; i < this.gcodes.length; i++) {
+            const deleted = this.gcodes[i]
+
+            if (deleted.id === id) {
+              this.gcodes.splice(i, 1)
+
+              // Toast for user
+              let toastHtml = ''
+              toastHtml += `<h6 class="text-danger">${deleted.filename} successfully deleted!</h6>`
+
+              this.$swal.Toast.fire({
+                // icon: 'success',
+                html: toastHtml,
+              })
+            }
+          }
+        })
+    },
+    renameItem(id, oldName, itemType = 'file') {
+      this.$swal.fire({
+        title: 'New name',
+        input: 'text',
+        inputValue: oldName,
+        inputPlaceholder: 'New name',
+        showCancelButton: true,
+        confirmButtonText: 'Save',
+        backdrop: 'rgba(0,0,0,0.5)',
+        preConfirm: async (newName) => {
+          if (!newName) {
+            this.$swal.showValidationMessage('Name is required')
+            return false;
+          }
+          if (itemType === 'folder' && this.folders.find(item => item.name === newName)) {
+            this.$swal.showValidationMessage('Folder with this name already exists')
+            return false;
+          }
+          try {
+            const url = itemType === 'file' ? urls.gcodeFile(id) : urls.gcodeFolder(id)
+            await axios.patch(url, `${itemType === 'file' ? 'filename' : 'name'}=${newName}`)
+          } catch (e) {
+            this.$swal.showValidationMessage('Server error')
+            console.log(e);
+            return false;
+          }
+          this.fetchFilesAndFolders()
+          return true;
+        },
+      })
+    },
+    deleteItem(id, itemType = 'file') {
+      this.$swal.Prompt.fire({
+        title: 'Are you sure?',
+        showCancelButton: true,
+        confirmButtonText: 'Yes',
+        cancelButtonText: 'No'
+      }).then(async userAction => {
+        if (userAction.isConfirmed) {
+          try {
+            const url = itemType === 'file' ? urls.gcodeFile(id) : urls.gcodeFolder(id)
+            await axios.delete(url)
+            this.fetchFilesAndFolders()
+          } catch (e) {
+            this.$swal.Reject.fire({
+              title: 'Error',
+              text: e.message,
+            })
+            console.log(e);
+          }
+        }
+      })
     },
     createFolder() {
       this.$swal.fire({
@@ -513,153 +626,18 @@ export default {
             console.log(e);
             return false;
           }
-          this.fetchGCodes();
+          this.fetchFilesAndFolders();
           return true;
         },
       })
     },
-    renameItem(id, oldName, itemType = 'file') {
-      this.$swal.fire({
-        title: 'New name',
-        input: 'text',
-        inputValue: oldName,
-        inputPlaceholder: 'New name',
-        showCancelButton: true,
-        confirmButtonText: 'Save',
-        backdrop: 'rgba(0,0,0,0.5)',
-        preConfirm: async (newName) => {
-          if (!newName) {
-            this.$swal.showValidationMessage('Name is required')
-            return false;
-          }
-          // if (this.folders.find(item => item.name === folderName)) {
-          //   this.$swal.showValidationMessage('Folder with this name already exists')
-          //   return false;
-          // }
-          try {
-            const url = itemType === 'file' ? urls.gcodeFile(id) : urls.gcodeFolder(id)
-            await axios.patch(url, `${itemType === 'file' ? 'filename' : 'name'}=${newName}`)
-          } catch (e) {
-            this.$swal.showValidationMessage('Server error')
-            console.log(e);
-            return false;
-          }
-          this.fetchGCodes();
-          return true;
-        },
-      })
-    },
-    deleteItem(id, itemType = 'file') {
-      this.$swal.Prompt.fire({
-        title: 'Are you sure?',
-        showCancelButton: true,
-        confirmButtonText: 'Yes',
-        cancelButtonText: 'No'
-      }).then(async userAction => {
-        if (userAction.isConfirmed) {
-          try {
-            const url = itemType === 'file' ? urls.gcodeFile(id) : urls.gcodeFolder(id)
-            await axios.delete(url)
-            this.fetchGCodes()
-          } catch (e) {
-            this.$swal.Reject.fire({
-              title: 'Error',
-              text: e.message,
-            })
-            console.log(e);
-          }
-        }
-      })
-    },
-    gcodeUploadSuccess() {
-      this.$refs.gcodesDropzone.removeAllFiles()
-
-      this.gcodes = []
-      this.fetchGCodes()
-    },
-
-    gcodeUploadError(file, message) {
-      this.$swal.Reject.fire({
-        html: `<p class="text-center">${message}</p>`})
-    },
-
-    async fetchGCodes() {
-      this.loading = true
-
-      this.gcodes = []
-      this.folders = []
-
-      let files
-      let folders
-
-      try {
-        files = await axios.get(urls.gcodeFiles({parentFolder: this.parentFolder}))
-        folders = await axios.get(urls.gcodeFolders({parentFolder: this.parentFolder}))
-      } catch (e) {
-        this.loading = false
-        console.error(e)
+    openFolder(folder) {
+      // Prevent navigation if main user action was to click dropdown item
+      if (document.querySelector('.swal2-container')) {
+        return false
       }
-
-      files = files?.data
-      folders = folders?.data
-
-      // console.log(folders)
-
-      if (files?.count) {
-        this.gcodes = files.results.map(data => normalizedGcode(data))
-      }
-
-      if (folders?.count) {
-        folders = folders.results.map(data => normalizedGcodeFolder(data))
-
-        for (let folder in folders) {
-          try {
-            const numFiles = (await axios.get(urls.gcodeFiles({parentFolder: folders[folder].id}))).data.count || 0
-            const numFolders = (await axios.get(urls.gcodeFolders({parentFolder: folders[folder].id}))).data.count || 0
-            folders[folder].numItems = numFiles + numFolders
-          } catch (e) {
-            this.loading = false
-            console.error(e)
-          }
-        }
-
-        this.folders = folders
-      }
-
-      this.loading = false
+      this.$router.push('/g_code_files/' + folder.id + '/')
     },
-
-    updateSorting(sortOption) {
-      if (this.activeSorting === sortOption) {
-        this.activeSortingDirection = -this.activeSortingDirection
-      } else {
-        this.activeSorting = sortOption
-        this.activeSortingDirection = SORTING_DIRECTIONS.ASC
-      }
-    },
-
-    removeItem(id) {
-      axios
-        .delete(urls.gcode(id), )
-        .then(() => {
-          for (let i = 0; i < this.gcodes.length; i++) {
-            const deleted = this.gcodes[i]
-
-            if (deleted.id === id) {
-              this.gcodes.splice(i, 1)
-
-              // Toast for user
-              let toastHtml = ''
-              toastHtml += `<h6 class="text-danger">${deleted.filename} successfully deleted!</h6>`
-
-              this.$swal.Toast.fire({
-                // icon: 'success',
-                html: toastHtml,
-              })
-            }
-          }
-        })
-    }
   },
 }
 </script>
@@ -728,9 +706,6 @@ export default {
     &:hover
       cursor: pointer
       background-color: var(--color-hover)
-
-    // &:nth-child(2n)
-    //   background-color: var(--color-table-accent)
 
     .item-info
       display: flex

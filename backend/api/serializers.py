8754679f8f2cb95ecade8ada7a1cb0d34c4404db
Kenneth Jiang <kenneth.jiang@gmail.com>
2022-12-11 20:04:08 -0800

    Turn out that Django/Python doesn't like DRY. Revert "Slight de-dup"
    This reverts commit b8ee73ae09cf29c4e9db04f3542475cc6c9e9d0b.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -94,6 +94,7 @@ class PrintSerializer(BasePrintSerializer):
     prediction_json_url = serializers.SerializerMethodField()
 
     class Meta:
+        model = Print
         fields = BasePrintSerializer.Meta.fields + ('printer', 'prediction_json_url', 'printshotfeedback_set',)
         read_only_fields = BasePrintSerializer.Meta.read_only_fields + ('printer', 'prediction_json_url', 'printshotfeedback_set',)
 
@@ -109,6 +110,7 @@ class PrinterSerializer(BasePrinterSerializer):
     current_print = BasePrintSerializer(read_only=True)
 
     class Meta:
+        model = Printer
         fields = BasePrinterSerializer.Meta.fields + ('pic', 'status', 'settings', 'current_print','normalized_p',)
         read_only_fields = BasePrinterSerializer.Meta.read_only_fields + ('pic', 'status', 'settings', 'current_print', 'normalized_p',)
 

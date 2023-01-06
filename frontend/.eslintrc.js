76e2a9f145af0c976a90a74ef3e9b127d73e4feb
Dmitry Puzyrev <dipuzyrev@gmail.com>
2023-01-06 23:26:48 +0600

    Edit eslint configuration

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -8,6 +8,6 @@ module.exports = {
   ],
   rules: {
     // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
+    'no-unused-vars': ["error", { "args": "none" }],
   }
 }

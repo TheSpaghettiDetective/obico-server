db20ae0c2a4aee50cfed2239757c3c5c20d31a38
lnjustin <lnjustin@users.noreply.github.com>
2023-02-05 20:56:13 -0500

    Update plugins.js

--- frontend/src/notifications/plugins.js
+++ frontend/src/notifications/plugins.js
@@ -1,30 +1,26 @@
 export default {
   email: {
     displayName: 'Email',
-    componentName: 'EmailPlugin',
   },
   twilio: {
     displayName: 'SMS',
-    componentName: 'TwilioPlugin',
   },
   pushbullet: {
     displayName: 'Pushbullet',
-    componentName: 'PushbulletPlugin',
   },
   discord: {
     displayName: 'Discord',
-    componentName: 'DiscordPlugin',
   },
   telegram: {
     displayName: 'Telegram',
-    componentName: 'TelegramPlugin',
   },
   pushover: {
     displayName: 'Pushover',
-    componentName: 'PushoverPlugin',
   },
   slack: {
     displayName: 'Slack',
-    componentName: 'SlackPlugin',
+  },
+  webhook: {
+    displayName: 'Webhook',
   },
 }

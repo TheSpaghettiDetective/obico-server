ee007f31e8ea237bede58eb566c9c854feb52c24
Dmitry Puzyrev <dipuzyrev@gmail.com>
2023-03-02 22:06:44 +0600

    Update duration formatting

--- frontend/src/lib/formatters.js
+++ frontend/src/lib/formatters.js
@@ -3,36 +3,34 @@ import moment from 'moment'
 // Possible result variants: 56s | 3m | 1h 56m | 2d 1h 56m
 export const humanizedDuration = (timeInSeconds) => {
   const seconds = Math.round(timeInSeconds || 0)
-  if (seconds < 60) {
-    return `${seconds}s`
-  }
-
   const components = getDurationComponents(seconds)
-  let result = ''
-  if (components.days !== 0) {
-    result += `${components.days}d `
-  }
-  if (components.days !== 0 || components.hours !== 0) {
-    result += `${components.hours}h `
-  }
 
-  result += `${components.minutes}m`
-  return result
+  if (components.days !== 0) {
+    return `${components.days}d ${components.hours}h`
+  } else if (components.hours !== 0) {
+    return `${components.hours}h ${components.minutes}m`
+  } else if (components.minutes !== 0) {
+    return `${components.minutes}m ${components.seconds}s`
+  } else {
+    return `${components.seconds}s`
+  }
 }
 
-export const getDurationComponents = (seconds) => {
-  const duration = moment.duration(seconds || 0, 'seconds')
+export const getDurationComponents = (durationInSeconds) => {
+  const duration = moment.duration(durationInSeconds || 0, 'seconds')
   const days = Math.floor(duration.asDays())
   const hours = duration.hours()
   const minutes = duration.minutes()
+  const seconds = duration.seconds()
   return {
     days,
     hours,
     minutes,
+    seconds,
   }
 }
 
-// Possible result variants: 0m | 1.2m | 1.23m
+// Example values: 0m | 1.2m | 1.23m
 export const humanizedFilamentUsage = (millimeters) => {
   const meters = (millimeters || 0) / 1000
   const twoDecimal = Math.round(meters * 100) / 100

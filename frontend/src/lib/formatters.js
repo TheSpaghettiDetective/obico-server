b27b41f92e91620bae4f07777787a0d3df22f3b9
Dmitry Puzyrev <dipuzyrev@gmail.com>
2023-03-03 11:33:42 +0600

    Handle edge cases for duration formatting

--- frontend/src/lib/formatters.js
+++ frontend/src/lib/formatters.js
@@ -6,9 +6,24 @@ export const humanizedDuration = (durationInSeconds) => {
   const components = getDurationComponents(seconds)
 
   if (components.days !== 0) {
-    return `${components.days}d ${components.hours + Math.round(components.minutes / 60)}h`
+    const hoursRounded = components.hours + Math.round(components.minutes / 60)
+    if (hoursRounded === 24) {
+      return `${components.days + 1}d ${0}h`
+    } else {
+      return `${components.days}d ${hoursRounded}h`
+    }
   } else if (components.hours !== 0) {
-    return `${components.hours}h ${components.minutes + Math.round(components.seconds / 60)}m`
+    const minutesRounded = components.minutes + Math.round(components.seconds / 60)
+    if (minutesRounded === 60) {
+      const hoursRounded = components.hours + 1
+      if (hoursRounded === 24) {
+        return `${1}d ${0}h`
+      } else {
+        return `${components.hours + 1}h ${0}m`
+      }
+    } else {
+      return `${components.hours}h ${minutesRounded}m`
+    }
   } else if (components.minutes !== 0) {
     return `${components.minutes}m ${components.seconds}s`
   } else {

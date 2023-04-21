2590cdd1b5d25ab292dcc6c0e7a1c7e18f359c42
John Northrup <john@8bitwizard.net>
2023-04-21 01:19:33 -0500

    Fix grammatical error and phrasing
    Clarifying error message through slight wording change / cleanup.

--- frontend/src/components/StreamingBox.vue
+++ frontend/src/components/StreamingBox.vue
@@ -397,7 +397,7 @@ export default {
       this.$swal.Prompt.fire({
         title: 'Video frames dropped',
         html: `
-          <p>The video frames are getting dropped because there is a bandwidth bottleneck along the route it they take to travel from your Raspberry Pi to your computer. The bottleneck can be anywhere but in most cases <Text bold>it's either your computer's internet connection, or your Raspberry Pi's</Text>.</p>
+          <p>The video frames are getting dropped because there is most likely a bandwidth bottleneck along the route they travel from your Raspberry Pi to your computer. The bottleneck can be anywhere, but in most cases, <Text bold>it is either your computer's internet connection, or your Raspberry Pi's</Text>.</p>
           <p>Make sure your computer is connected to the same network as your Pi. If you still see this warning, you need to trouble-shoot your computer's Wi-Fi connection, probably by moving closer to the Wi-Fi router.</p>
           <p>If the webcam stream is smooth when your computer is on the same Wi-Fi network as your Pi, the bottleneck is likely with the upload speed of your internet connection. You need to run a speed test to make sure you have high-enough upload speed, as well as <b>low latency (ping)</b>.</p>
           <p>Check out <a target="_blank" href="https://www.obico.io/docs/user-guides/webcam-feed-is-laggy/">the step-by-step trouble-shooting guide.</a></p>

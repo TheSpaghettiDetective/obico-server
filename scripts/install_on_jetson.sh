d48f90918a4d62665d283abc55eda4e7cd45078e
Kenneth Jiang <kenneth.jiang@gmail.com>
2023-05-21 17:26:33 -0700

    Simplify image building process

--- scripts/install_on_jetson.sh
+++ scripts/install_on_jetson.sh
@@ -1,12 +1,9 @@
 #!/bin/bash -e
 
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
-   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-sudo apt update && sudo apt install -y nvidia-docker2 python python-pip curl libffi-dev python-openssl libssl-dev zlib1g-dev gcc g++ make
+sudo apt update && sudo apt install -y libhdf5-dev libssl-dev python3-pip
 sudo systemctl restart docker
-sudo python -m pip install --upgrade pip setuptools wheel
-sudo python -m pip install docker-compose
+sudo pip3 install --upgrade pip setuptools wheel
+sudo pip3 install docker-compose
 
 cat <<EOT >docker-compose.override.yml
 version: '2.4'

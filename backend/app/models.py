deeaa56389d48f2840edde5c8e64655733ef7a66
Kenneth Jiang <kenneth.jiang@gmail.com>
2023-07-22 08:39:33 -0700

    Give default webcam a name so that it's easier for the frontend to handle

--- backend/app/models.py
+++ backend/app/models.py
@@ -149,7 +149,7 @@ class Printer(SafeDeleteModel):
         (NONE, 'Just notify me'),
         (PAUSE, 'Pause the printer and notify me'),
     )
-    DEFAULT_WEBCAM_SETTINGS = {'flipV': False, 'flipH': False, 'rotation': 0, 'streamRatio': '16:9'}
+    DEFAULT_WEBCAM_SETTINGS = {'flipV': False, 'flipH': False, 'rotation': 0, 'streamRatio': '16:9', 'name': 'Default'}
 
     name = models.CharField(max_length=256, null=False)
     auth_token = models.CharField(max_length=256, unique=True, null=False, blank=False)

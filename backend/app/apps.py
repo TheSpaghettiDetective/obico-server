fee0a8e188ed484aa77b5cc21c545f31c2dc38e2
Alex Wardle <alex@teched-out.com>
2023-10-09 14:21:06 -0600

    fix django-channels-presence
    See https://github.com/mitmedialab/django-channels-presence/issues/19

--- backend/app/apps.py
+++ backend/app/apps.py
@@ -1,5 +1,10 @@
+from channels_presence.apps import RoomsConfig
 from django.apps import AppConfig
 
 
 class AppConfig(AppConfig):
     name = 'app'
+
+
+class CustomRoomsConfig(RoomsConfig):
+    name = 'channels_presence'

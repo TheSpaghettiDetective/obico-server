8e62c7feb22bc93ce62a44dc5772bcb4b6c3ae81
ApexArray <91706106+ApexArray@users.noreply.github.com>
2023-10-26 16:48:46 -0600

    fix logging check

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -237,7 +237,7 @@ LOGGING = {
     }
 }
 
-if get_bool('DISABLE_DEBUG_QUERY_LOGGING', False):
+if not get_bool('DISABLE_DEBUG_QUERY_LOGGING', False):
     LOGGING['loggers']['django.db.backends'] = {
         'level': 'DEBUG',
         'handlers': ['console_debug'],

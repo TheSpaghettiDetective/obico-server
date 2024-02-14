4344801a5b60bb4ea774b1fb9a8d8fba4825c2bf
Michał Sochoń <kaszpir@gmail.com>
2024-02-14 20:39:03 +0100

    Allow defining where darknet is located
    Decrease wget spam and fetch urls from file directly
    
    Pin pip version
    
    Run basic test if the app is able to run with gunicorn
    
    Disable creating .pyc files in read-only containers

--- ml_api/lib/darknet.py
+++ ml_api/lib/darknet.py
@@ -126,9 +126,12 @@ class YoloNet:
 lib = None
 using_gpu = False
 
+DARKNET_PATH=os.getenv('DARKNET_PATH', '../darknet')
+print(f"DARKNET_PATH={DARKNET_PATH}")
+
 print('\n')
 try:
-    so_path = os.path.join('/darknet', "libdarknet_gpu.so")
+    so_path = os.path.join(DARKNET_PATH, "libdarknet_gpu.so")
     print(f"Let's try darknet lib built with GPU support - {so_path}")
     lib = CDLL(so_path, RTLD_GLOBAL)
     print(f"Done! Hooray! Now we have darknet with GPU support.")
@@ -137,7 +140,7 @@ try:
 except Exception as e:
     print(f"Nope! Failed to load darknet lib built with GPU support. erors={e}")
 
-    so_path = os.path.join('/darknet', "libdarknet_cpu.so")
+    so_path = os.path.join(DARKNET_PATH, "libdarknet_cpu.so")
     print(f"Now let's try darknet lib on CPU - {so_path}")
     lib = CDLL(so_path, RTLD_GLOBAL)
     print(f"Done! Darknet is now running on CPU.")

aac4adce8c191ece042381eea511127eb8096114
kui <105092010+kuiwang2022@users.noreply.github.com>
2024-03-15 11:06:02 +0800

    feat: i18n config test

--- frontend/src/i18n/i18n.js
+++ frontend/src/i18n/i18n.js
@@ -1,8 +1,11 @@
 import i18n from 'i18next';
 
 import baseEn from './base/en.json';
+import baseZh from './base/zh.json';
 import yumiEn from './yumi/en.json';
+import yumiZh from './yumi/zh.json';
 import mintionEn from './mintion/en.json';
+import mintionZh from './mintion/zh.json';
 import { syndicate } from '@src/lib/page-context'
 
 
@@ -10,26 +13,49 @@ const getCurrentSyndicate = () => {
   return syndicate().provider
 };
 
-const currentSyndicate = getCurrentSyndicate();
+const getLocalizationlanguage = () => {
+  let language = 'en'
+  if(window){
+    let stragety = {
+      'en': 'en',
+      'zh-CN': 'zh',
+    };
+    const userLanguage = navigator.language || navigator.userLanguage ||'en';
+    language = stragety[userLanguage] || 'en'
+  }
+  return language
 
+}
+
+const currentSyndicate = getCurrentSyndicate();
+const language = getLocalizationlanguage()
 
 const resources = {
   'en_base': {
     translation: baseEn,
   },
+  'zh_base': {
+    translation: baseZh,
+  },
   'en_yumi': {
     translation: yumiEn,
   },
+  'zh_yumi': {
+    translation: yumiZh,
+  },
   'en_mintion': {
     translation: mintionEn,
+  },
+  'zh_mintion': {
+    translation: mintionZh,
   },
 };
 
 i18n
   .init({
     resources,
-    lng: `en_${currentSyndicate}`,
-    fallbackLng: 'en_base',
+    lng: `${language}_${currentSyndicate}`,
+    fallbackLng: `${language}_base`,
     interpolation: {
       escapeValue: false,
     },

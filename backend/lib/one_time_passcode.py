df5d7e0767be84a21851515fe17464505e0bd18e
Kenneth Jiang <kenneth.jiang@gmail.com>
2024-04-05 21:24:24 -0700

    Use lower case so that O and 0 are easier to differentiate

--- backend/lib/one_time_passcode.py
+++ backend/lib/one_time_passcode.py
@@ -19,7 +19,7 @@ def request_one_time_passcode(one_time_passcode: str, code_length=5) -> str:
     verification_code = lookup_value_by_one_time_passcode(one_time_passcode)
     if verification_code is None: # one_time_passcode not existed in cache
         letters_and_digits = string.ascii_letters + string.digits
-        new_code = ''.join(random.choice(letters_and_digits) for _ in range(code_length)).upper()
+        new_code = ''.join(random.choice(letters_and_digits) for _ in range(code_length)).lower()
         set_value_by_one_time_passcode(new_code, 60*60*2, '') # one time passcode has a ttl of 2 hours
         return (new_code, '')
     else:

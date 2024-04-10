ad4ffe3a54d441e29f763d091bd3906f2f014b45
kui <105092010+kuiwang2022@users.noreply.github.com>
2024-04-10 20:39:10 +0800

    feat: add printer

--- frontend/src/views/printer-wizard/TargetPlatformSelectionPage.vue
+++ frontend/src/views/printer-wizard/TargetPlatformSelectionPage.vue
@@ -16,47 +16,67 @@
         <div class="row">
           <h3 class="col-sm-12 text-center p-3">{{ $t("Which platform are you using?") }}</h3>
         </div>
-        <div v-if="devicesWithObicoPreInstalled.length > 0" class="row">
-          <div class="col-sm-12 col-lg-6 p-4">
-            <h4 class="col-sm-12 p-3">{{ $t("Devices with Obico Pre-installed") }}</h4>
-            <div v-for="item in devicesWithObicoPreInstalled" :key="item.id" @click="targetPlatformClicked('klipper-obico-enabled')">
-              <div>{{ item.model }}</div>
+        <b-row class="center mt-3">
+          <div class="col-sm-12 col-lg-8"><PrinterProgress :step="0"></PrinterProgress></div>
+        </b-row>
+        <div v-if="devicesWithObicoPreInstalled.length > 0" class="row mt-4">
+          <div class="col-sm-12 col-lg-6 py-4">
+            <h4 class="py-3">{{ $t("Devices with Obico Pre-installed") }}</h4>
+            <div class="printer-list">
+              <div v-for="item in devicesWithObicoPreInstalled" :key="item.id" @click="targetPlatformClicked('klipper-obico-enabled',item)" class="printer-item">
+                <div class="img-wrap">
+                  <img :src="item.image_url" alt="">
+                </div>
+                <div class="item-label">{{ item.model }}</div>
+              </div>
             </div>
+            
           </div>
         </div>
-        <div class="row">
-          <div class="col-sm-12 col-lg-6 p-4">
-            <div class="wizard-card" @click="targetPlatformClicked('klipper-preinstalled')">
-              <div>
+        <div class="wizard-card-list">
+          <div class="wizard-card-horizontal" @click="targetPlatformClicked('klipper-preinstalled')">
+              <div class="img-wrap">
                 <img :src="require('@static/img/klipper_logo.jpg')" />
               </div>
-              <h3 class="mt-4">{{ $t("Pre-Installed Klipper Printer") }}</h3>
-              <h5>{{ $t("Creality K1, Sonic Pad, Sovol SV07, Kingroon KLP1, Elegoo Neptune 4, etc...") }}</h5>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-sm-12 col-lg-6 p-4">
-            <div class="wizard-card" @click="targetPlatformClicked('klipper-generic')">
-              <div>
-                <img :src="require('@static/img/klipper_logo.jpg')" />
-                <img :src="require('@static/img/mainsail_logo.png')" />
-                <img :src="require('@static/img/fluidd_logo.png')" />
+              <div class="text-wrap">
+                <h3 >{{ $t("Pre-Installed Klipper Printer") }}</h3>
+                <h5>{{ $t("Creality K1, Sonic Pad, Sovol SV07, Kingroon KLP1, Elegoo Neptune 4, etc...") }}</h5>
               </div>
-              <h3 class="mt-4">{{ $t("Generic Klipper - Self Installed") }}</h3>
-              <h5>{{ $t("If you installed Klipper yourself on a Raspberry Pi or other linux device. E.g., Voron, RatRig") }}</h5>
+            
             </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-sm-12 col-lg-6 p-4">
-            <div class="wizard-card" @click="targetPlatformClicked('octoprint')">
-              <img :src="require('@static/img/octoprint_logo.png')" />
-              <h3 class="mt-4">OctoPrint</h3>
-              <div>{{ $t("Including OctoPrint for Klipper such as OctoKlipper.") }}</div>
+          
+        
+            <div class="wizard-card-horizontal" @click="targetPlatformClicked('klipper-generic')">
+              <div class="img-wrap">
+                <img :src="require('@static/img/klipper_logo.jpg')" />
+                <div class="img-tip">
+                  <img :src="require('@static/img/mainsail_logo.png')" />
+                  <img :src="require('@static/img/fluidd_logo.png')" />
+                </div>
+                
+              </div>
+              <div class="text-wrap">
+                <h3 >{{ $t("Generic Klipper - Self Installed") }}</h3>
+                <h5>{{ $t("If you installed Klipper yourself on a Raspberry Pi or other linux device. E.g., Voron, RatRig") }}</h5>
+              </div>
+             
             </div>
-          </div>
+          
+        
+            <div class="wizard-card-horizontal" @click="targetPlatformClicked('octoprint')">
+              <div class="img-wrap">
+                <img :src="require('@static/img/octoprint_logo.png')" />
+              </div>
+              
+              <div class="text-wrap">
+                <h3 >OctoPrint</h3>
+                <h5>{{ $t("Including OctoPrint for Klipper such as OctoKlipper.") }}</h5>
+              </div>
+            </div>
         </div>
+        
+            
+          
       </div>
     </template>
   </page-layout>
@@ -64,10 +84,11 @@
 
 <script>
 import PageLayout from '@src/components/PageLayout.vue'
-
+import PrinterProgress from '../../components/printers/wizard/PrinterProgress.vue';
 export default {
   components: {
     PageLayout,
+    PrinterProgress
   },
   data() {
     return {
@@ -84,11 +105,12 @@ export default {
     this.devicesWithObicoPreInstalled = await response.json();
   },
   methods: {
-    targetPlatformClicked(platform) {
+    targetPlatformClicked(platform,printerItem) {
       this.$router.push({
         path: `/printers/wizard/guide/${platform}/`,
         query: {
           ...this.$route.query,
+          printerItem:printerItem?JSON.stringify(printerItem):undefined
         }
       });
     },
@@ -96,22 +118,93 @@ export default {
 }
 </script>
 
-<style lang="sass" scoped>
-.wizard-card
-  background-color: var(--color-surface-primary)
-  border-radius: var(--border-radius-lg)
-  padding: 1em
-  display: flex
-  flex-direction: column
-  align-items: center
-  min-height: 24em
-  justify-content: center
-  text-align: center
-  &:hover
-    cursor: pointer
-    color: var(--color-primary)
-    background-color: var(--color-hover)
-.wizard-card img
-  height: 3em
-  margin: 1em
+<style lang="scss" scoped>
+.container{
+  background-color:#2D3E4F;
+  padding:70px 140px;
+  border-radius:16px;
+  margin-top:60px;
+  .printer-list{
+    display:flex;
+    gap:20px;
+    .printer-item{
+      .img-wrap{
+        padding:20px;
+        background:#485B71;
+        border-radius:6px;
+        img{
+          width:100px;
+          height:auto;
+        }
+      }
+      .item-label{
+        margin-top:3px;
+        text-align:center;
+      }
+    }
+  }
+    
+}
+  
+
+
+.wizard-card{
+  background-color: var(--color-surface-primary);
+  border-radius: var(--border-radius-lg);
+  padding: 1em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 24em;
+  justify-content: center;
+  text-align: center;
+  &:hover{
+    cursor: pointer;
+    color: var(--color-primary);
+    background-color: var(--color-hover);
+  }
+  img{
+    height: 3em;
+    margin: 1em;
+  }
+}
+.wizard-card-list{
+  margin-top:40px;
+  .wizard-card-horizontal{
+    margin-top:20px;
+    display:flex;
+    align-items:center;
+    gap:30px;
+    background-color: var(--color-surface-primary);
+    border-radius: var(--border-radius-md);
+    padding:20px;
+    cursor: pointer;
+  
+  .img-wrap{
+    img{
+      width: 5em;
+    }
+    .img-tip{
+      display:flex;
+      justify-content:space-between;
+      img{
+        width:2em;
+      }
+    }
+  }
+  .text-wrap{
+    h5{
+      margin-top:10px;
+      color:#AAACB0;
+      font-size:16px;
+    }
+  }
+
+}
+}
+
+
+  
+  
+ 
 </style>

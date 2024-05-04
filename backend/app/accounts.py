5fa87129e08713db7e1b1c3a704b9c2181cac400
Kenneth Jiang <kenneth.jiang@gmail.com>
2024-05-04 15:56:53 -0700

    Revert "Normal signin and login working as expected"
    This reverts commit 28b2d90ecb5deca14c2ce2b3862c3a9c9470eddd.

--- backend/app/accounts.py
+++ backend/app/accounts.py
@@ -5,6 +5,32 @@ from django.contrib.sites.shortcuts import get_current_site
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 
+class SiteSpecificBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        if username is None:
+            username = kwargs.get('email')
+        UserModel = get_user_model()
+        if request is not None:
+            site = get_current_site(request)
+            try:
+                user = UserModel.objects.get(email=username, site=site)
+                if user.check_password(password):
+                    return user
+            except UserModel.DoesNotExist:
+                return None
+        return None
+
+
+class SiteSpecificAccountAdapter(DefaultAccountAdapter):
+    def is_unique_email(self, email):
+        return not self.user_model.objects.filter(email=email, site=self.request.site).exists()
+
+    def save_user(self, request, user, form, commit=True):
+        user.site = get_current_site(request)
+        user.username = f'{user.email}_{user.site.id}'
+        return super().save_user(request, user, form, commit)
+
+
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
     def pre_social_login(self, request, sociallogin):
         # Ignore existing social accounts, just do this stuff for new ones
@@ -35,7 +61,7 @@ class SocialAccountAdapter(DefaultSocialAccountAdapter):
         # check if given email address already exists.
         # Note: __iexact is used to ignore cases
         try:
-            email_address = EmailAddress.objects.get(email__iexact=email, site=request.current_site)  # FIXME verified=True?
+            email_address = EmailAddress.objects.get(email__iexact=email)  # FIXME verified=True?
         # if it does not, let allauth take care of this new social account
         except EmailAddress.DoesNotExist:
             return
--- backend/app/customBackendAuth.py
+++ backend/app/customBackendAuth.py
@@ -1,33 +1,18 @@
-# from allauth.account.auth_backends import AuthenticationBackend
-from django.contrib.auth.backends import ModelBackend
-from django.contrib.sites.models import Site
+from allauth.account.auth_backends import AuthenticationBackend
 from django.contrib.auth import get_user_model
 from django.contrib.sites.shortcuts import get_current_site
 
-class CustomAuthenticationBackend(ModelBackend):
-
+class CustomAuthenticationBackend(AuthenticationBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         if username is None:
             username = kwargs.get('email')
-            password = kwargs.get('password')
-        self.site = get_current_site(request)
-        self.email = username
         UserModel = get_user_model()
-        try:
-            # Retrieve user based on email and site
-            user = UserModel.objects.get(email=username, site=self.site)
-            if user.check_password(password):
-                return user
-            return None
-        except UserModel.DoesNotExist:
-            return None
-    
-    def get_user(self, user_id):
-        """
-        Overrides the get_user method to allow users to log in using their email address.
-        """
-        UserModel = get_user_model()
-        try:
-            return UserModel.objects.get(pk=user_id)
-        except UserModel.DoesNotExist:
-            return None
+        if request is not None:
+            site = get_current_site(request)
+            try:
+                user = UserModel.objects.get(email=username, site=site)
+                if user.check_password(password):
+                    return user
+            except UserModel.DoesNotExist:
+                return None
+        return None

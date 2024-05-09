8494177b2282985dc601cb4ac0683585fb2a94ae
alielshimy <ali.elshimy@breadfast.com>
2024-05-09 20:56:50 +0300

    change email query to lower case the input

--- backend/app/accounts.py
+++ backend/app/accounts.py
@@ -18,7 +18,7 @@ class SiteSpecificBackend(ModelBackend):
         if request is not None:
             syndicate = get_current_site(request).syndicates.first()
             try:
-                user = UserModel.objects.get(email=username, syndicate=syndicate)
+                user = UserModel.objects.get(email=username.lower(), syndicate=syndicate)
                 if user.check_password(password):
                     return user
             except UserModel.DoesNotExist:

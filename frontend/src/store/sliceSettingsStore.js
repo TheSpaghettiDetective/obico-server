d89ce224826ce484c2a2ed365f05207ee518e772
sajal <sajalregmi4@gmail.com>
2024-07-31 01:38:51 -0700

    Scale works

--- frontend/src/store/sliceSettingsStore.js
+++ frontend/src/store/sliceSettingsStore.js
@@ -1,31 +1,89 @@
 const state = {
-  bottomSheetOpen: false,
+  RotationbottomSheetOpen: false,
+  ScalebottomSheetOpen: false,
+  MovebottomSheetOpen: false,
   rotationAngles: [0, 0, 0],
+  initialDimensions: { x: 0, y: 0, z: 0 },
+  currentDimensions: { x: 0, y: 0, z: 0 },
+
 }
 
 const mutations = {
-  SET_BOTTOM_SHEET_OPEN(state, bottomSheetOpen) {
-    state.bottomSheetOpen = bottomSheetOpen
+
+  SET_INITIAL_DIMENSIONS(state, dimensions) {
+    Object.assign(state.initialDimensions, dimensions);
+  },
+  SET_CURRENT_DIMENSIONS(state, dimensions) {
+    Object.assign(state.currentDimensions, dimensions);
+  },
+
+
+
+  SET_ROTATION_BOTTOM_SHEET_OPEN(state, RotationbottomSheetOpen) {
+    state.RotationbottomSheetOpen = RotationbottomSheetOpen
   },
   SET_ROTATION_ANGLE(state, {index, angle}) {
     state.rotationAngles.splice(index, 1, angle);
-  }
+  },
+  SET_SCALE_BOTTOM_SHEET_OPEN(state, ScalebottomSheetOpen) {
+    state.ScalebottomSheetOpen = ScalebottomSheetOpen
+  },
+  SET_MOVE_BOTTOM_SHEET_OPEN(state, MovebottomSheetOpen) { 
+    state.MovebottomSheetOpen = MovebottomSheetOpen
+  },
+
 }
 
 const actions = {
+
+  setInitialDimensions({ commit }, dimensions) {
+    commit('SET_INITIAL_DIMENSIONS', dimensions);
+    commit('SET_CURRENT_DIMENSIONS', dimensions); // Initialize current dimensions without triggering watchers
+  },
+  updateCurrentDimensions({ commit }, dimensions) {
+    commit('SET_CURRENT_DIMENSIONS', dimensions);
+  },
+
+
+
+  //RotationBottomSheet
   openModelRotationBottomSheet({ commit }) {
-    commit('SET_BOTTOM_SHEET_OPEN', true)
+    commit('SET_ROTATION_BOTTOM_SHEET_OPEN', true)
   },
   closeModelRotationBottomSheet({ commit }) {
-    commit('SET_BOTTOM_SHEET_OPEN', false)
+    commit('SET_ROTATION_BOTTOM_SHEET_OPEN', false)
   },
+
+  //Scale Bottom Sheet
+  openModelScaleBottomSheet({ commit }) {
+    commit('SET_SCALE_BOTTOM_SHEET_OPEN', true)
+  },
+  closeModelScaleBottomSheet({ commit }) {
+    commit('SET_SCALE_BOTTOM_SHEET_OPEN', false)
+  },
+
+  //Move Bottom Sheet
+  openMoveBottomSheet({ commit }) { 
+    commit('SET_MOVE_BOTTOM_SHEET_OPEN', true)
+  },
+  closeMoveBottomSheet({ commit }) { 
+    commit('SET_MOVE_BOTTOM_SHEET_OPEN', false)
+  },
+
+
+  //Rotation Angle
   updateRotationAngle({ commit }, payload) {
     commit('SET_ROTATION_ANGLE', payload)
   },
+
 }
 
 const getters = {
-  bottomSheetOpen: (state) => state.bottomSheetOpen,
+  RotationbottomSheetOpen: (state) => state.RotationbottomSheetOpen,
+  ScalebottomSheetOpen: (state) => state.ScalebottomSheetOpen,
+  MovebottomSheetOpen: (state) => state.MovebottomSheetOpen,
+  initialDimensions: (state) => state.initialDimensions,
+  currentDimensions: (state) => state.currentDimensions,
   rotationAngles: (state) => state.rotationAngles,
 }
 

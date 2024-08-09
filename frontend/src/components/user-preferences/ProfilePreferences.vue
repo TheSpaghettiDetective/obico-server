793c77df95e3c7732508807c59a259eac2bf1667
Kenneth Jiang <kenneth.jiang@gmail.com>
2024-08-09 11:42:51 -0700

    Make the page more viewable for mobile app

--- frontend/src/components/user-preferences/ProfilePreferences.vue
+++ frontend/src/components/user-preferences/ProfilePreferences.vue
@@ -29,17 +29,22 @@
         </saving-animation>
       </div>
     </div>
-    <div v-if="!onlyName" class="row">
-      <label for="id_email" class="col-md-2 col-sm-3 col-form-label">{{ $t("Primary Email") }}</label>
-      <div class="col-md-10 col-sm-9 col-form-label text-muted">
-        {{ user.email }} ({{ user.is_primary_email_verified ? 'Verified' : 'Unverified' }})
-        <div class="form-text"><a href="/accounts/email">{{ $t("Manage email addresses") }}</a></div>
+    <div class="pt-4" v-if="!onlyName">
+      <h2 class="section-title">{{ $t("Email") }}</h2>
+      <div class="row">
+        <label for="id_email" class="col-md-2 col-sm-3 col-form-label">{{ $t("Primary Email") }}</label>
+        <div class="col-md-10 col-sm-9 col-form-label text-muted">
+          {{ user.email }} ({{ user.is_primary_email_verified ? 'Verified' : 'Unverified' }})
+          <div class="form-text"><a href="/accounts/email">{{ $t("Manage email addresses") }}</a></div>
+        </div>
       </div>
     </div>
-    <div v-if="!onlyName" class="form-group row">
-      <label class="col-md-2 col-sm-3 col-form-label">{{ $t("Password") }}</label>
-      <div class="col-md-10 col-sm-9 col-form-label text-muted">
-        <a href="/accounts/password/change">{{ $t("Change") }}</a>
+    <div class="pt-4" v-if="!onlyName">
+      <h2 class="section-title">{{ $t("Password") }}</h2>
+      <div class="form-group row">
+        <div class="col-md-10 col-sm-9 col-form-label text-muted">
+          <a href="/accounts/password/change">{{ $t("Change Password") }}</a>
+        </div>
       </div>
     </div>
   </section>

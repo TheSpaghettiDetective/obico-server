d3758bf5ab914bcb159c25f5194a15b9a41706cd
Kenneth Jiang <kenneth.jiang@gmail.com>
2025-01-06 18:05:46 -0800

    Prebuild weave in base image

--- scripts/build_dockerfile_web_base.sh
+++ scripts/build_dockerfile_web_base.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-docker buildx build backend --push --platform linux/arm64/v8,linux/amd64 -f backend/Dockerfile.base -t thespaghettidetective/web:base-$1
+BUILDKIT_MAX_PARALLELISM=1 docker buildx build backend --progress=plain --push --platform linux/arm64/v8,linux/amd64 -f backend/Dockerfile.base -t thespaghettidetective/web:base-$1

8f5d52c444af1458560c921d924d677ae365b491
Kenneth Jiang <kenneth.jiang@gmail.com>
2025-01-07 20:41:08 -0800

    Revert "Prebuild weave in base image"
    This reverts commit d3758bf5ab914bcb159c25f5194a15b9a41706cd.

--- scripts/build_dockerfile_web_base.sh
+++ scripts/build_dockerfile_web_base.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-BUILDKIT_MAX_PARALLELISM=1 docker buildx build backend --progress=plain --push --platform linux/arm64/v8,linux/amd64 -f backend/Dockerfile.base -t thespaghettidetective/web:base-$1
+docker buildx build backend --push --platform linux/arm64/v8,linux/amd64 -f backend/Dockerfile.base -t thespaghettidetective/web:base-$1

245f8b8706c5efca1dfc973858b2d2f3ce4ccb34
Daniel McKnight <daniel@mcknight.tech>
2025-07-02 21:42:17 -0700

    Update backend container build and docker-compose

---
+++ backend/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+if [ "${OBICO_CONTAINER}" = "tasks" ]; then
+  celery -A config worker --beat -l info -c 2 -Q realtime,celery
+elif [ "${OBICO_CONTAINER}" = "web" ]; then
+  python manage.py migrate && python manage.py collectstatic -v 2 --noinput && daphne -b 0.0.0.0 -p 3334 config.routing:application
+fi

e2a725404919c5b82c13a2091c5de5d373d68813
Kenneth Jiang <kenneth.jiang@gmail.com>
2025-12-25 09:48:31 -0800

    Refactor API headers to include Accept-Language for improved localization support

--- frontend/src/views/jusprin/v1.2/lib/api.js
+++ frontend/src/views/jusprin/v1.2/lib/api.js
@@ -1,25 +1,25 @@
 import axios from 'axios'
+import i18n from '@src/i18n/i18n'
+
+const getHeaders = (authToken) => ({
+  Authorization: `Bearer ${authToken}`,
+  'Accept-Language': i18n.language,
+})
 
 export default {
   get: (url, authToken) => {
     return axios.get(url, {
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-      },
+      headers: getHeaders(authToken),
     })
   },
   post: (url, authToken, data) => {
     return axios.post(url, data, {
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-      },
+      headers: getHeaders(authToken),
     })
   },
   put: (url, authToken, data) => {
     return axios.put(url, data, {
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-      },
+      headers: getHeaders(authToken),
     })
   },
 }
